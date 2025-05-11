686e0658305dded127203ec0f8707c0dba363ac6
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-05-11 10:04:49 +0300

    [feat] InputDialogWindow

---
+++ Editor/InputDialogWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CustomUtils.Editor.EditorTheme;
+using UnityEditor;
+
+namespace CustomUtils.Editor
+{
+    internal sealed class InputDialogWindow : WindowBase
+    {
+        internal string Message { get; set; }
+        internal string InputText { get; set; }
+
+        internal event Action<string> OnComplete;
+
+        protected override void DrawWindowContent()
+        {
+            EditorVisualControls.LabelField(Message);
+
+            InputText = EditorGUILayout.PasswordField(string.Empty, InputText);
+
+            EditorGUILayout.BeginHorizontal();
+            if (EditorVisualControls.Button("OK"))
+            {
+                OnComplete?.Invoke(InputText);
+                Close();
+            }
+
+            if (EditorVisualControls.Button("Cancel"))
+            {
+                OnComplete?.Invoke("");
+                Close();
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
