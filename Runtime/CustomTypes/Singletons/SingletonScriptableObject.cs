44d7208678143ead33c5b25eeb2d083b4a1a05cb
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-05-17 15:38:14 +0300

    [feat] ScriptableObjectLoader

--- Runtime/CustomTypes/Singletons/SingletonScriptableObject.cs
+++ Runtime/CustomTypes/Singletons/SingletonScriptableObject.cs
@@ -1,68 +1,63 @@
-using System.IO;
 using System.Reflection;
 using CustomUtils.Runtime.AssetLoader;
-using UnityEditor;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace CustomUtils.Runtime.CustomTypes.Singletons
 {
+    /// <inheritdoc />
     /// <summary>
-    /// Base class for ScriptableObjects that follow the Singleton pattern.
-    /// Requires a ResourceAttribute to specify the asset location.
-    /// Automatically creates the asset in the editor if it doesn't exist.
+    /// Base class for singleton ScriptableObjects that are automatically loaded or created when accessed.
     /// </summary>
-    /// <typeparam name="T">The type of ScriptableObject to make a singleton.</typeparam>
+    /// <typeparam name="T">The type of ScriptableObject to create as a singleton.</typeparam>
     public abstract class SingletonScriptableObject<T> : ScriptableObject where T : ScriptableObject
     {
         private static T _instance;
 
-        public static T Instance
+        /// <summary>
+        /// Gets the singleton instance of the ScriptableObject.
+        /// If the instance doesn't exist, it will be loaded from resources or created if necessary.
+        /// The location is determined by the ResourceAttribute applied to the class.
+        /// </summary>
+        [UsedImplicitly] public static T Instance
         {
             get
             {
                 if (_instance)
                     return _instance;
 
+#if UNITY_EDITOR
                 var attribute = typeof(T).GetCustomAttribute<ResourceAttribute>();
                 if (attribute == null)
                 {
-                    Debug.LogWarning($"[SingletonScriptableObject::Instance] {typeof(T).Name} missing ResourceAttribute");
+                    Debug.LogWarning(
+                        $"[SingletonScriptableObject::Instance] {typeof(T).Name} missing ResourceAttribute");
                     return null;
                 }
 
-                if (attribute.TryGetFullResourcePath(out var path))
-                    _instance = Resources.Load<T>(path);
+                // ReSharper disable once ConvertIfStatementToConditionalTernaryExpression
+                if (attribute.IsEditorResource)
+                    _instance = ScriptableObjectLoader<T>.LoadEditorResource();
+                else
+#endif
+                    _instance = ScriptableObjectLoader<T>.Load();
 
 #if UNITY_EDITOR
-                if (_instance)
-                    return _instance;
-
-                _instance = CreateInstance<T>();
-                SaveInstance(attribute);
+                if (!_instance)
+                    _instance = ScriptableObjectLoader<T>.CreateAndSaveAsset();
 #endif
+
                 return _instance;
             }
         }
 
 #if UNITY_EDITOR
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-        private static void ResetStaticVariables() => _instance = null;
-#endif
-
-#if UNITY_EDITOR
-        private static void SaveInstance(ResourceAttribute attribute)
+        private static void ResetStaticVariables()
         {
-            var name = string.IsNullOrEmpty(attribute.Name) ? typeof(T).Name : attribute.Name;
-            var assetPath = string.IsNullOrEmpty(attribute.FullPath)
-                ? $"Assets/{name}.asset"
-                : $"{attribute.FullPath}/{name}.asset";
+            _instance = null;
 
-            var directory = Path.GetDirectoryName(assetPath);
-            if (string.IsNullOrEmpty(directory) is false && Directory.Exists(directory) is false)
-                Directory.CreateDirectory(directory);
-
-            AssetDatabase.CreateAsset(_instance, assetPath);
-            AssetDatabase.SaveAssets();
+            ScriptableObjectLoader<T>.RemoveFromCache();
         }
 #endif
     }
