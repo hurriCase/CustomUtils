e94618badb4ebb92e475dfcb6486dd4f1acf79c5
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-05-24 20:06:05 +0300

    [feat] ListExtensions/ProgressExtensions/(TryGetFileVersion/TryGetFileContentHash for StringExtensionsEditor)

--- Editor/Extensions/StringExtensionsEditor.cs
+++ Editor/Extensions/StringExtensionsEditor.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
 using JetBrains.Annotations;
 using UnityEditor;
 
@@ -36,5 +38,50 @@ namespace CustomUtils.Editor.Extensions
         /// <returns>The backing field name format</returns>
         public static string ConvertToBackingField(this string propertyName)
             => $"<{propertyName}>k__BackingField";
+
+        /// <summary>
+        /// Attempts to calculate an MD5 hash for the specified file's content.
+        /// </summary>
+        /// <param name="filePath">The path to the file to hash.</param>
+        /// <param name="hash">When this method returns, contains the calculated MD5 hash as a lowercase hex string, or an empty string if the file doesn't exist.</param>
+        /// <returns>True if the hash was calculated successfully; otherwise, false.</returns>
+        /// <remarks>
+        /// The hash is returned as a lowercase hexadecimal string without dashes.
+        /// </remarks>
+        public static bool TryGetFileContentHash(this string filePath, out string hash)
+        {
+            hash = string.Empty;
+            if (File.Exists(filePath) is false)
+                return false;
+
+            using var md5 = MD5.Create();
+
+            using var stream = File.OpenRead(filePath);
+
+            var hashBytes = md5.ComputeHash(stream);
+            hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to generate a version string for a file based on its last write time and content hash.
+        /// </summary>
+        /// <param name="filePath">The path to the file.</param>
+        /// <param name="version">When this method returns, contains a version string in the format "yyyyMMdd-[first 8 chars of hash]", or an empty string if the file doesn't exist.</param>
+        /// <returns>True if the version was generated successfully; otherwise, false.</returns>
+        /// <remarks>
+        /// The version combines the file's last modification date with the first 8 characters of its content hash.
+        /// </remarks>
+        public static bool TryGetFileVersion(this string filePath, out string version)
+        {
+            version = string.Empty;
+            if (filePath.TryGetFileContentHash(out var hash) is false)
+                return false;
+
+            var fileInfo = new FileInfo(filePath);
+
+            version = $"{fileInfo.LastWriteTime:yyyyMMdd}-{hash[..8]}";
+            return true;
+        }
     }
 }
---
+++ Runtime/Extensions/ListExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace CustomUtils.Runtime.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="List{T}"/> collections.
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Gets an element at the specified index, or creates and adds default elements
+        /// to the list until the index is accessible, then returns the element at that index.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="list">The list to extend or retrieve from.</param>
+        /// <param name="index">The zero-based index of the element to get or create.</param>
+        /// <param name="defaultValue">The default value to use when creating new elements.
+        /// If not specified, uses the default value for type T.</param>
+        /// <returns>The element at the specified index.</returns>
+        [UsedImplicitly]
+        public static T GetOrCreate<T>(this List<T> list, int index, T defaultValue = default)
+        {
+            while (list.Count <= index)
+            {
+                list.Add(defaultValue);
+            }
+
+            return list[index];
+        }
+    }
+}
