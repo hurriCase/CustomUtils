e1f9e24e0c7b6747629ff1942d8cdcd5ca98ddca
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-05-31 10:19:20 +0300

    [refactor] xml comment/[UsedImplicitly] for PoolHandler

--- Runtime/PoolHandler.cs
+++ Runtime/PoolHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Pool;
 using Object = UnityEngine.Object;
@@ -6,6 +7,11 @@ using Object = UnityEngine.Object;
 // ReSharper disable MemberCanBeInternal
 namespace CustomUtils.Runtime
 {
+    /// <summary>
+    /// Generic object pool handler for Unity objects with automatic activation management
+    /// </summary>
+    /// <typeparam name="TPoolEntity">Type of Unity object to pool (GameObject or Component)</typeparam>
+    [UsedImplicitly]
     public class PoolHandler<TPoolEntity> where TPoolEntity : Object
     {
         private TPoolEntity _prefab;
@@ -17,6 +23,18 @@ namespace CustomUtils.Runtime
 
         private IObjectPool<TPoolEntity> _pool;
 
+        /// <summary>
+        /// Initializes the object pool with specified configuration
+        /// </summary>
+        /// <param name="prefab">Prefab to instantiate for new pool objects</param>
+        /// <param name="defaultPoolSize">Initial number of objects to create in the pool</param>
+        /// <param name="maxPoolSize">Maximum number of objects the pool can hold</param>
+        /// <param name="onCreateCallback">Called when a new object is created</param>
+        /// <param name="onGetCallback">Called when an object is retrieved from the pool</param>
+        /// <param name="onReleaseCallback">Called when an object is returned to the pool</param>
+        /// <param name="onDestroyCallback">Called when an object is destroyed</param>
+        /// <param name="parent">Transform parent for instantiated objects</param>
+        [UsedImplicitly]
         public void Init(
             TPoolEntity prefab,
             int defaultPoolSize = 10,
@@ -44,6 +62,11 @@ namespace CustomUtils.Runtime
                 maxPoolSize);
         }
 
+        /// <summary>
+        /// Creates a new entity instance from the prefab
+        /// </summary>
+        /// <returns>New entity instance</returns>
+        [UsedImplicitly]
         protected virtual TPoolEntity CreateEntity()
         {
             var entity = Object.Instantiate(_prefab, _parent);
@@ -52,28 +75,57 @@ namespace CustomUtils.Runtime
             return entity;
         }
 
+        /// <summary>
+        /// Called when an entity is retrieved from the pool
+        /// </summary>
+        /// <param name="entity">Entity being retrieved</param>
+        [UsedImplicitly]
         protected virtual void OnGet(TPoolEntity entity)
         {
             _onGetCallback?.Invoke(entity);
             SetActive(entity, true);
         }
 
+        /// <summary>
+        /// Called when an entity is returned to the pool
+        /// </summary>
+        /// <param name="entity">Entity being returned</param>
+        [UsedImplicitly]
         protected virtual void OnRelease(TPoolEntity entity)
         {
             _onReleaseCallback?.Invoke(entity);
             SetActive(entity, false);
         }
 
+        /// <summary>
+        /// Called when an entity is being destroyed
+        /// </summary>
+        /// <param name="entity">Entity being destroyed</param>
+        [UsedImplicitly]
         protected virtual void OnDestroy(TPoolEntity entity)
         {
             _onDestroyCallback?.Invoke(entity);
             Object.Destroy(entity);
         }
 
+        /// <summary>
+        /// Gets an object from the pool
+        /// </summary>
+        /// <returns>Active pooled object</returns>
+        [UsedImplicitly]
         public TPoolEntity Get() => _pool.Get();
 
+        /// <summary>
+        /// Returns an object to the pool
+        /// </summary>
+        /// <param name="element">Object to return to the pool</param>
+        [UsedImplicitly]
         public void Release(TPoolEntity element) => _pool.Release(element);
 
+        /// <summary>
+        /// Clears all objects from the pool
+        /// </summary>
+        [UsedImplicitly]
         public void Clear() => _pool.Clear();
 
         private void SetActive(TPoolEntity entity, bool active)
