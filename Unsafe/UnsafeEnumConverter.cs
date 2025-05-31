309d717f9a757a18d004c5b384ca590225d76636
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-05-31 08:23:30 +0300

    [feat] UnsafeEnumConverter

---
+++ Unsafe/UnsafeEnumConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace CustomUtils.Unsafe.CustomUtils.Unsafe
+{
+    /// <summary>
+    /// Fast enum to int conversion using unsafe code. Zero allocations.
+    /// </summary>
+    /// <typeparam name="T">Enum type (must be 4-byte enum like int/uint)</typeparam>
+    [UsedImplicitly]
+    public static class UnsafeEnumConverter<T> where T : unmanaged, Enum
+    {
+        /// <summary>
+        /// Converts enum to int. No validation performed.
+        /// </summary>
+        /// <param name="value">Enum value to convert</param>
+        /// <returns>Integer representation of the enum</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [UsedImplicitly]
+        public static unsafe int ToInt32(T value) => *(int*)&value;
+
+        /// <summary>
+        /// Converts int to enum. No validation performed.
+        /// </summary>
+        /// <param name="value">Integer value to convert</param>
+        /// <returns>Enum with the specified integer value</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [UsedImplicitly]
+        public static unsafe T FromInt32(int value) => *(T*)&value;
+    }
+}
