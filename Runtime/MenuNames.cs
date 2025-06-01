0c339b9d38f3fe4cf4221993135c4312fec3add7
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-01 07:20:35 +0300

    [remove] unused MenuNames.cs

--- Runtime/MenuNames.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CustomUtils.Runtime
-{
-    internal static class MenuNames
-    {
-        private const string AudioPath = "Sound/";
-
-        internal const string DatabasePath = AudioPath + "Database/";
-    }
-}
