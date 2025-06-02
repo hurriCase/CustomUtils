3a13e7204210707018d5536501d1a6aa99b946c5
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-02 17:40:19 +0300

    [feat] ResourceReferences

---
+++ Runtime/ResourceReferences.cs
@@ -0,0 +1,12 @@
+﻿using CustomUtils.Runtime.AssetLoader;
+using CustomUtils.Runtime.CustomTypes.Singletons;
+using UnityEngine;
+
+namespace CustomUtils.Runtime
+{
+    [Resource(ResourcePaths.ResourceReferencesFullPath, ResourcePaths.ResourceReferencesAssetName)]
+    internal sealed class ResourceReferences : SingletonScriptableObject<ResourceReferences>
+    {
+        [field: SerializeField] internal Sprite SquareSprite { get; private set; }
+    }
+}
