80d59bd3bc27760cb53757a66cf813d0c863365d
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-04 20:19:24 +0300

    [refactor] RectTransformExtendedEditor.cs to match build-in RectTransformEditor

--- Editor/UI/RectTransformExtendedEditor.cs
+++ Editor/UI/RectTransformExtendedEditor.cs
@@ -12,14 +12,20 @@ namespace CustomUtils.Editor.UI
         private UnityEditor.Editor _defaultEditor;
 
         [SerializeField] private float _parentWidth;
+        [SerializeField] private float _parentHeight;
+
         [SerializeField] private float _leftMarginWidth;
         [SerializeField] private float _rightMarginWidth;
-        [SerializeField] private float _contentWidth;
-
-        [SerializeField] private float _parentHeight;
         [SerializeField] private float _topMarginHeight;
         [SerializeField] private float _bottomMarginHeight;
+
+        [SerializeField] private float _contentWidth;
         [SerializeField] private float _contentHeight;
+
+        [SerializeField] private bool _showCustomProperties = false;
+
+        private static readonly MethodInfo _multiFieldPrefixLabelMethod;
+        private static readonly MethodInfo _calcPrefixLabelWidthMethod;
 
         protected override void InitializeEditor()
         {
@@ -27,6 +33,8 @@ namespace CustomUtils.Editor.UI
             var rectTransformEditorType = assembly.GetType("UnityEditor.RectTransformEditor");
 
             _defaultEditor = CreateEditor(targets, rectTransformEditorType);
+
+            _showCustomProperties = EditorPrefs.GetBool("RectTransformExtended.showLayoutHelper", false);
         }
 
         protected override void CleanupEditor()
@@ -44,6 +52,8 @@ namespace CustomUtils.Editor.UI
 
             serializedObject.Update();
 
+            EditorGUILayout.Space();
+
             DrawCustomSections();
 
             serializedObject.ApplyModifiedProperties();
@@ -51,12 +61,36 @@ namespace CustomUtils.Editor.UI
 
         protected override void DrawCustomSections()
         {
-            DrawFoldoutSection("Width", DrawWidthSection);
-            DrawFoldoutSection("Height", DrawHeightSection);
+            var foldoutStyle = EditorVisualControls.CreateTextStyle(
+                EditorStyles.foldout,
+                EditorStyles.foldout.fontSize,
+                FontStyle.Normal);
 
-            if (EditorVisualControls.Button("Apply anchors") is false)
-                return;
+            EditorVisualControls.Foldout("Layout Helper", ref _showCustomProperties, () =>
+            {
+                ++EditorGUI.indentLevel;
 
+                DrawVector2FieldLabelAbove("Parent", ref _parentWidth, ref _parentHeight, "Width", "Height");
+                DrawVector2FieldLabelAbove("Content", ref _contentWidth, ref _contentHeight, "Width", "Height");
+
+                EditorGUILayout.Space();
+
+                DrawVector2Field("Left/Right Margin", ref _leftMarginWidth, ref _rightMarginWidth, "L", "R");
+                DrawVector2Field("Top/Bottom Margin", ref _topMarginHeight, ref _bottomMarginHeight, "T", "B");
+
+                --EditorGUI.indentLevel;
+
+                EditorGUILayout.Space();
+
+                EditorVisualControls.Button("Apply anchors", () => ApplyAnchors(),
+                    GUILayout.Height(EditorGUIUtility.singleLineHeight));
+            }, foldoutStyle);
+
+            EditorPrefs.SetBool("RectTransformExtended.showLayoutHelper", _showCustomProperties);
+        }
+
+        private void ApplyAnchors()
+        {
             foreach (var rectTransformTarget in targets)
             {
                 var rectTransform = (RectTransform)rectTransformTarget;
@@ -76,20 +110,88 @@ namespace CustomUtils.Editor.UI
             }
         }
 
-        private void DrawWidthSection()
+        static RectTransformExtendedEditor()
         {
-            _parentWidth = EditorStateControls.FloatField("Parent Width", _parentWidth);
-            _leftMarginWidth = EditorStateControls.FloatField("Left Margin Width", _leftMarginWidth);
-            _rightMarginWidth = EditorStateControls.FloatField("Right Margin Width", _rightMarginWidth);
-            _contentWidth = EditorStateControls.FloatField("Content Width", _contentWidth);
+            var editorGUIType = typeof(EditorGUI);
+
+            _multiFieldPrefixLabelMethod = editorGUIType.GetMethod("MultiFieldPrefixLabel",
+                BindingFlags.Static | BindingFlags.NonPublic,
+                null, new[] { typeof(Rect), typeof(int), typeof(GUIContent), typeof(int) }, null);
+
+            _calcPrefixLabelWidthMethod = editorGUIType.GetMethod("CalcPrefixLabelWidth",
+                BindingFlags.Static | BindingFlags.NonPublic,
+                null, new[] { typeof(GUIContent), typeof(GUIStyle) }, null);
         }
 
-        private void DrawHeightSection()
+        private void DrawVector2FieldLabelAbove(string mainLabel, ref float xValue, ref float yValue,
+            string xLabel = "X", string yLabel = "Y")
         {
-            _parentHeight = EditorStateControls.FloatField("Parent Height", _parentHeight);
-            _topMarginHeight = EditorStateControls.FloatField("Top Margin Height", _topMarginHeight);
-            _bottomMarginHeight = EditorStateControls.FloatField("Bottom Margin Height", _bottomMarginHeight);
-            _contentHeight = EditorStateControls.FloatField("Content Height", _contentHeight);
+            var controlRect = EditorGUILayout.GetControlRect(true, EditorGUIUtility.singleLineHeight * 2f);
+            controlRect.height = EditorGUIUtility.singleLineHeight;
+
+            var labelRect = new Rect(controlRect.x, controlRect.y, EditorGUIUtility.labelWidth, controlRect.height);
+            EditorVisualControls.LabelField(labelRect, mainLabel);
+
+            var firstColumnRect = GetColumnRect(controlRect, 0);
+            var secondColumnRect = GetColumnRect(controlRect, 1);
+
+            firstColumnRect.height = EditorGUIUtility.singleLineHeight * 2f;
+            secondColumnRect.height = EditorGUIUtility.singleLineHeight * 2f;
+
+            var xLabelRect = new Rect(firstColumnRect.x, firstColumnRect.y, firstColumnRect.width,
+                EditorGUIUtility.singleLineHeight);
+            var xFieldRect = new Rect(firstColumnRect.x, firstColumnRect.y + EditorGUIUtility.singleLineHeight,
+                firstColumnRect.width, EditorGUIUtility.singleLineHeight);
+
+            var yLabelRect = new Rect(secondColumnRect.x, secondColumnRect.y, secondColumnRect.width,
+                EditorGUIUtility.singleLineHeight);
+            var yFieldRect = new Rect(secondColumnRect.x, secondColumnRect.y + EditorGUIUtility.singleLineHeight,
+                secondColumnRect.width, EditorGUIUtility.singleLineHeight);
+
+            EditorVisualControls.LabelField(xLabelRect, xLabel);
+            EditorVisualControls.LabelField(yLabelRect, yLabel);
+
+            xValue = EditorStateControls.FloatField(xFieldRect, $"{mainLabel} {xLabel}", xValue);
+            yValue = EditorStateControls.FloatField(yFieldRect, $"{mainLabel} {yLabel}", yValue);
+        }
+
+        private void DrawVector2Field(string label, ref float xValue, ref float yValue, string xLabel = "X",
+            string yLabel = "Y")
+        {
+            var controlRect = EditorGUILayout.GetControlRect(true, EditorGUIUtility.singleLineHeight);
+            var controlId = GUIUtility.GetControlID(label.GetHashCode(), FocusType.Keyboard, controlRect);
+
+            var fieldRect = (Rect)_multiFieldPrefixLabelMethod.Invoke(null,
+                new object[] { controlRect, controlId, new GUIContent(label), 2 });
+
+            var originalLabelWidth = EditorGUIUtility.labelWidth;
+            var originalIndentLevel = EditorGUI.indentLevel;
+
+            var subLabelWidth = (float)_calcPrefixLabelWidthMethod.Invoke(null,
+                new object[] { new GUIContent(xLabel), EditorStyles.label });
+
+            EditorGUI.indentLevel = 0;
+            EditorGUIUtility.labelWidth = subLabelWidth;
+
+            var columnWidth = (fieldRect.width - 2f) / 2f;
+
+            var xRect = new Rect(fieldRect.x, fieldRect.y, columnWidth, fieldRect.height);
+            var yRect = new Rect(fieldRect.x + columnWidth + 2f, fieldRect.y, columnWidth, fieldRect.height);
+
+            xValue = EditorStateControls.FloatField(xRect, $"{label} {xLabel}", new GUIContent(xLabel), xValue);
+            yValue = EditorStateControls.FloatField(yRect, $"{label} {yLabel}", new GUIContent(yLabel), yValue);
+
+            EditorGUIUtility.labelWidth = originalLabelWidth;
+            EditorGUI.indentLevel = originalIndentLevel;
+        }
+
+        private Rect GetColumnRect(Rect totalRect, int column)
+        {
+            totalRect.xMin += EditorGUIUtility.labelWidth - 1f;
+            var columnRect = totalRect;
+            columnRect.xMin += (float)((totalRect.width - 4.0) * (column / 3.0)) + column * 2;
+            columnRect.width = (float)((totalRect.width - 4.0) / 3.0);
+            return columnRect;
         }
     }
 }
