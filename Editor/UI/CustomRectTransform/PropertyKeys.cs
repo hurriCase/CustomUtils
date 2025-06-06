1c0d9586973c98a7e85068cdb68a9638eed1faa3
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-06 08:18:16 +0300

    [fix] ParentHeightKey name

--- Editor/UI/CustomRectTransform/PropertyKeys.cs
+++ Editor/UI/CustomRectTransform/PropertyKeys.cs
@@ -4,7 +4,7 @@
     {
         internal const string ShowPropertiesKey = "RectTransformExtended.showProperties";
         internal const string ParentWidthKey = "RectTransformExtended.parentWidth";
-        internal const string ParentHeightKey = "RectTransformExtended.arentHeight";
+        internal const string ParentHeightKey = "RectTransformExtended.parentHeight";
         internal const string LeftMarginWidthKey = "RectTransformExtended.leftMarginWidth";
         internal const string RightMarginWidthKey = "RectTransformExtended.rightMarginWidth";
         internal const string TopMarginHeightKey = "RectTransformExtended.topMarginHeight";
