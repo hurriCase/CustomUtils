e48a420c3886790885268bb0924cdd05520d3084
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-06 07:45:41 +0300

    [feat] persistent editor

---
+++ Editor/PersistentEditor/PersistentPropertyExtensions.cs
@@ -0,0 +1,34 @@
+﻿using CustomUtils.Editor.Extensions;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace CustomUtils.Editor.PersistentEditor
+{
+    /// <summary>
+    /// Extension methods for creating persistent editor properties
+    /// </summary>
+    [UsedImplicitly]
+    public static class PersistentPropertyExtensions
+    {
+        /// <summary>
+        /// Creates a persistent editor property that automatically saves to EditorPrefs
+        /// </summary>
+        /// <typeparam name="TProperty">Type of the property value</typeparam>
+        /// <param name="target">Target object to create unique key for</param>
+        /// <param name="key">Base key for storage</param>
+        /// <param name="defaultValue">Default value if no saved value exists</param>
+        /// <returns>New persistent editor property</returns>
+        [UsedImplicitly]
+        public static PersistentEditorProperty<TProperty> CreatePersistentProperty<TProperty>(
+            this Object target,
+            string key,
+            TProperty defaultValue = default)
+        {
+            var uniqueKey = !target
+                ? target.GetObjectUniqueKey(key) ?? key
+                : key;
+
+            return new PersistentEditorProperty<TProperty>(uniqueKey, defaultValue);
+        }
+    }
+}
