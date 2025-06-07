fe7475e039b836f1842975be3fdb6a6b32e4be68
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-07 14:46:04 +0300

    [feat] ComponentExtensions

---
+++ Runtime/Extensions/ComponentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for Unity Component objects.
+    /// </summary>
+    [UsedImplicitly]
+    public static class ComponentExtensions
+    {
+        /// <summary>
+        /// Attempts to find a component of type T in the parent objects of the specified component.
+        /// </summary>
+        /// <typeparam name="T">The type of component to search for, must inherit from Component.</typeparam>
+        /// <param name="component">The component to start the search from.</param>
+        /// <param name="result">When this method returns, contains the component of type T if found; otherwise, null.</param>
+        /// <returns>
+        /// <c>true</c> if a component of type T was found in the parent hierarchy; otherwise, <c>false</c>.
+        /// </returns>
+        [UsedImplicitly]
+        public static bool TryGetComponentInParent<T>(this Component component, out T result) where T : Component
+        {
+            result = component.GetComponentInParent<T>();
+            return result;
+        }
+    }
+}
