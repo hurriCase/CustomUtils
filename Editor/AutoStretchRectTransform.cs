92dbfe294ffc9789d456164ad4ad82a5fbbb2796
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-08 18:11:43 +0300

    [feat] AutoStretchRectTransform

---
+++ Editor/AutoStretchRectTransform.cs
@@ -0,0 +1,27 @@
+﻿#if IS_STRETCH_RECTRANSFORM
+using UnityEditor;
+using UnityEngine;
+
+namespace CustomUtils.Editor
+{
+    [InitializeOnLoad]
+    public class AutoStretchRectTransform
+    {
+        static AutoStretchRectTransform()
+        {
+            ObjectFactory.componentWasAdded += OnComponentAdded;
+        }
+
+        private static void OnComponentAdded(Component component)
+        {
+            if (!component || component is not RectTransform rectTransform)
+                return;
+
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+            rectTransform.offsetMin = Vector2.zero;
+            rectTransform.offsetMax = Vector2.zero;
+        }
+    }
+}
+#endif
