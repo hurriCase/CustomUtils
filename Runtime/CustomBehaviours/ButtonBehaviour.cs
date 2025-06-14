1f7f6866494f6e1d325d388965e678b38c699bab
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-14 18:33:23 +0300

    [feat] ButtonBehaviour

---
+++ Runtime/CustomBehaviours/ButtonBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CustomUtils.Runtime.CustomBehaviours
+{
+    [UsedImplicitly]
+    public class ButtonBehaviour : MonoBehaviour
+    {
+        [UsedImplicitly]
+        public Button TextComponent =>
+            _buttonComponent = _buttonComponent ? _buttonComponent : GetComponent<Button>();
+        private Button _buttonComponent;
+    }
+}
