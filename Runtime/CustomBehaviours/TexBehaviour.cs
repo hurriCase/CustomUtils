181b3ee7f8bea1dffb4b2c2f58a024a94110c687
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-14 16:52:24 +0300

    [feat] TextBehaviour

---
+++ Runtime/CustomBehaviours/TexBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using TMPro;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.CustomBehaviours
+{
+    [UsedImplicitly]
+    public class TexBehaviour : MonoBehaviour
+    {
+        [UsedImplicitly]
+        public TextMeshProUGUI TextComponent =>
+            _textComponent = _textComponent ? _textComponent : GetComponent<TextMeshProUGUI>();
+        private TextMeshProUGUI _textComponent;
+    }
+}
