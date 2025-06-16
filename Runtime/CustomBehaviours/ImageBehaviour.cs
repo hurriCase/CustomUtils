10b3d861a0229abdcd5749d8c255e6ff6cfde8b4
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-16 07:49:59 +0300

    [feat] ImageBehaviour

---
+++ Runtime/CustomBehaviours/ImageBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CustomUtils.Runtime.CustomBehaviours
+{
+    [UsedImplicitly]
+    public class ImageBehaviour : MonoBehaviour
+    {
+        [UsedImplicitly]
+        protected Image Image => _image = _image ? _image : GetComponent<Image>();
+        private Image _image;
+    }
+}
