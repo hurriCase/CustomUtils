b4682938772f3b1a268eca6ec5225bb9aa20cad3
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-18 10:40:23 +0300

    [feat] RectTransformBehaviour

---
+++ Runtime/CustomBehaviours/RectTransformBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.CustomBehaviours
+{
+    [UsedImplicitly]
+    public class RectTransformBehaviour : MonoBehaviour
+    {
+        [UsedImplicitly]
+        protected RectTransform RectTransform =>
+            _rectTransform = _rectTransform ? _rectTransform : GetComponent<RectTransform>();
+        private RectTransform _rectTransform;
+    }
+}
