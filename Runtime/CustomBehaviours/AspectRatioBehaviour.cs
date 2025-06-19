9a2b8d7fa50cc74f47281727986f62350a1ea481
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-19 06:23:50 +0300

    [feat] AspectRatioBehaviour

---
+++ Runtime/CustomBehaviours/AspectRatioBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CustomUtils.Runtime.CustomBehaviours
+{
+    [UsedImplicitly]
+    public class AspectRatioBehaviour : MonoBehaviour
+    {
+        [UsedImplicitly]
+        public AspectRatioFitter AspectRatioFitter => _aspectRatioFitter =
+            _aspectRatioFitter ? _aspectRatioFitter : GetComponent<AspectRatioFitter>();
+        private AspectRatioFitter _aspectRatioFitter;
+    }
+}
