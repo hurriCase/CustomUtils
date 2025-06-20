cbd4dfc2b6b173592cf3a9836f731d857acf7ee1
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-20 20:24:43 +0300

    [refactor] make ZLinqExtensions.cs publicly accessible

--- Runtime/Extensions/ZLinqExtensions.cs
+++ Runtime/Extensions/ZLinqExtensions.cs
@@ -3,18 +3,18 @@ using ZLinq;
 
 namespace CustomUtils.Runtime.Extensions
 {
-    internal static class ZLinqExtensions
+    public static class ZLinqExtensions
     {
         /// <summary>
         /// Gets a random element from the ZLinq ValueEnumerable using Unity's Random number generator.
         /// </summary>
         /// <typeparam name="TEnumerator">The type of the enumerator.</typeparam>
-        /// <typeparam name="T">The type of elements in the enumerable.</typeparam>
+        /// <typeparam name="TSource">The type of elements in the enumerable.</typeparam>
         /// <param name="source">The ZLinq ValueEnumerable to get a random element from.</param>
         /// <returns>A randomly selected element from the enumerable.</returns>
         [UsedImplicitly]
-        public static T Random<TEnumerator, T>(this ValueEnumerable<TEnumerator, T> source)
-            where TEnumerator : struct, IValueEnumerator<T>
+        public static TSource Random<TEnumerator, TSource>(this ValueEnumerable<TEnumerator, TSource> source)
+            where TEnumerator : struct, IValueEnumerator<TSource>
         {
             var count = source.Count();
             var randomIndex = UnityEngine.Random.Range(0, count);
