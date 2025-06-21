56574a35045cc4e3bc2f7c1155c69c2077ed3a5a
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-21 07:18:22 +0300

    [feat] RandomInt/RandomFloat

---
+++ Runtime/CustomTypes/Randoms/RandomFloat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace CustomUtils.Runtime.CustomTypes.Randoms
+{
+    /// <summary>
+    /// Represents a random float value generator with configurable minimum and maximum bounds.
+    /// </summary>
+    [Serializable]
+    public struct RandomFloat
+    {
+        [SerializeField] private float _randomStart;
+        [SerializeField] private float _randomEnd;
+
+        /// <summary>
+        /// Gets a random float value between the specified minimum and maximum bounds.
+        /// </summary>
+        /// <value>A random float value in the range [_randomStart, _randomEnd].</value>
+        [UsedImplicitly]
+        public float RandomValue => Random.Range(_randomStart, _randomEnd);
+
+        /// <summary>
+        /// Initializes a new instance of the RandomFloat struct with the specified range.
+        /// </summary>
+        /// <param name="randomStart">The minimum value (inclusive) for the random range.</param>
+        /// <param name="randomEnd">The maximum value (inclusive) for the random range.</param>
+        public RandomFloat(float randomStart, float randomEnd)
+        {
+            _randomStart = randomStart;
+            _randomEnd = randomEnd;
+        }
+    }
+}
---
+++ Runtime/CustomTypes/Randoms/RandomInt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace CustomUtils.Runtime.CustomTypes.Randoms
+{
+    /// <summary>
+    /// Represents a random integer value generator with configurable minimum and maximum bounds.
+    /// </summary>
+    [Serializable]
+    public struct RandomInt
+    {
+        [SerializeField] private int _randomStart;
+        [SerializeField] private int _randomEnd;
+
+        /// <summary>
+        /// Gets a random integer value between the specified minimum and maximum bounds.
+        /// </summary>
+        /// <value>A random integer value in the range [_randomStart, _randomEnd).</value>
+        [UsedImplicitly]
+        public int RandomValue => Random.Range(_randomStart, _randomEnd);
+
+        /// <summary>
+        /// Initializes a new instance of the RandomInt struct with the specified range.
+        /// </summary>
+        /// <param name="randomStart">The minimum value (inclusive) for the random range.</param>
+        /// <param name="randomEnd">The maximum value (exclusive) for the random range.</param>
+        public RandomInt(int randomStart, int randomEnd)
+        {
+            _randomStart = randomStart;
+            _randomEnd = randomEnd;
+        }
+    }
+}
