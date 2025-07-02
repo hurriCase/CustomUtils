0a55ad6c1d46b5165c6511246af6963801cada0c
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-07-02 18:22:02 +0300

    Revert "[project] remove ratio layout system"
    This reverts commit 8c5be531cbc7f450e2f788ebe66a363145e46d39.

---
+++ Runtime/UI/RatioLayout/LayoutDirection.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace CustomUtils.Runtime.UI.RatioLayout
+{
+    [UsedImplicitly]
+    public enum LayoutDirection
+    {
+        Horizontal = 0,
+        Vertical = 1
+    }
+}
