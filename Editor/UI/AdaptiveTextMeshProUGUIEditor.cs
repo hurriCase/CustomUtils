d981b5aca8d366b05dc94fe4e579c796ea2d90fe
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-07-05 07:37:32 +0300

    [feat] AdaptiveTextMeshProUGUI

---
+++ Editor/UI/AdaptiveTextMeshProUGUIEditor.cs
@@ -0,0 +1,35 @@
+﻿using CustomUtils.Editor.CustomEditorUtilities;
+using CustomUtils.Runtime.UI;
+using TMPro.EditorUtilities;
+using UnityEditor;
+
+namespace CustomUtils.Editor.UI
+{
+    [CustomEditor(typeof(AdaptiveTextMeshProUGUI), true)]
+    [CanEditMultipleObjects]
+    internal sealed class AdaptiveTextMeshProUGUIEditor : TMP_EditorPanelUI
+    {
+        private EditorStateControls _editorStateControls;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            _editorStateControls = new EditorStateControls(target, serializedObject);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            serializedObject.Update();
+
+            EditorVisualControls.LabelField("Adaptive Settings");
+
+            _editorStateControls.PropertyField(nameof(AdaptiveTextMeshProUGUI.BaseFontSize));
+            _editorStateControls.PropertyField(nameof(AdaptiveTextMeshProUGUI.ReferenceWidth));
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Runtime/UI/AdaptiveTextMeshProUGUI.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.UI
+{
+    public sealed class AdaptiveTextMeshProUGUI : TextMeshProUGUI
+    {
+        [field: SerializeField] public float BaseFontSize { get; private set; }
+        [field: SerializeField] public float ReferenceWidth { get; private set; }
+
+        protected override void OnRectTransformDimensionsChange()
+        {
+            base.OnRectTransformDimensionsChange();
+
+            SetCategoriesText();
+        }
+
+        private void SetCategoriesText()
+        {
+            var adaptiveFontSize = BaseFontSize * rectTransform.rect.width / ReferenceWidth;
+
+            fontSize = adaptiveFontSize;
+        }
+    }
+}
