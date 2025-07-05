cf90f8606d044a9ff544cedf7b8e452705efdd0d
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-07-05 16:12:23 +0300

    [refactor] DynamicRatioLayoutElement.cs simplification

--- Runtime/UI/DynamicRatioLayoutElement.cs
+++ Runtime/UI/DynamicRatioLayoutElement.cs
@@ -14,8 +14,6 @@ namespace CustomUtils.Runtime.UI
         [SerializeField] private DimensionType _dimensionType;
         [SerializeField] private float _parentReferenceSize;
         [SerializeField] private float _otherContentReferenceSize;
-        [SerializeField] private LayoutGroup _ratioLayoutGroup;
-        [SerializeField] private RectOffset _padding;
 
         private RectTransform RectTransform =>
             _rectTransform = _rectTransform ? _rectTransform : GetComponent<RectTransform>();
@@ -23,17 +21,18 @@ namespace CustomUtils.Runtime.UI
 
         private void OnRectTransformDimensionsChange()
         {
-            HandleChanges();
+            UpdateAspectRatio();
         }
 
-        private void Update()
+        private void OnValidate()
         {
-            HandleChanges();
+            UpdateAspectRatio();
         }
 
-        private void HandleChanges()
+        private void UpdateAspectRatio()
         {
-            if (_dimensionType == DimensionType.None)
+            if (!_referenceObject || _parentReferenceSize <= 0 || _otherContentReferenceSize <= 0 ||
+                _dimensionType == DimensionType.None)
                 return;
 
             var currentSize = _dimensionType switch
@@ -43,53 +42,18 @@ namespace CustomUtils.Runtime.UI
                 _ => 0f
             };
 
-            if (currentSize <= 0f || _parentReferenceSize <= 0f)
+            if (currentSize <= 0f)
                 return;
 
-            var differenceRatio = _parentReferenceSize / currentSize;
-
-            UpdateMargins(differenceRatio);
-            UpdateAspectRatio(differenceRatio);
-        }
-
-        private void UpdateMargins(float differenceRatio)
-        {
-            if (!_ratioLayoutGroup || _dimensionType == DimensionType.None)
-                return;
-
-            switch (_dimensionType)
-            {
-                case DimensionType.Width:
-                    _ratioLayoutGroup.padding.top = (int)(_padding.top / differenceRatio);
-                    _ratioLayoutGroup.padding.bottom = (int)(_padding.bottom / differenceRatio);
-                    break;
-
-                case DimensionType.Height:
-                    _ratioLayoutGroup.padding.left = (int)(_padding.left / differenceRatio);
-                    _ratioLayoutGroup.padding.right = (int)(_padding.right / differenceRatio);
-                    break;
-
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        private void UpdateAspectRatio(float differenceRatio)
-        {
-            if (!_referenceObject || _parentReferenceSize == 0 || _otherContentReferenceSize == 0 ||
-                _dimensionType == DimensionType.None)
-                return;
-
-            // ReSharper disable once SwitchExpressionHandlesSomeKnownEnumValuesWithExceptionInDefault
             var (referenceObjectSize, size) = _dimensionType switch
             {
-                DimensionType.Width => (_referenceObject.rect.height, RectTransform.rect.width),
-                DimensionType.Height => (_referenceObject.rect.width, RectTransform.rect.height),
+                DimensionType.Width => (_referenceObject.rect.height, currentSize),
+                DimensionType.Height => (_referenceObject.rect.width, currentSize),
                 _ => throw new ArgumentOutOfRangeException()
             };
 
-            var currentOtherContentWidth = _otherContentReferenceSize / differenceRatio;
-            var newRatio = (currentOtherContentWidth + referenceObjectSize) / size;
+            var scaledOtherContentSize = _otherContentReferenceSize * currentSize / _parentReferenceSize;
+            var newRatio = (scaledOtherContentSize + referenceObjectSize) / size;
 
             if (newRatio <= 0)
                 return;
