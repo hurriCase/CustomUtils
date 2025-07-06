ff1f5ff011f79c1ce4d3444eca485790c6386972
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-07-06 17:24:38 +0300

    [feat] EnumArray

---
+++ Editor/EnumArrayDrawer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using CustomUtils.Runtime.CustomTypes;
+using UnityEditor;
+using UnityEngine;
+
+namespace CustomUtils.Editor
+{
+    [CustomPropertyDrawer(typeof(EnumArray<,>))]
+    public class EnumArrayDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var valuesProperty = property.FindPropertyRelative("_values");
+            if (valuesProperty == null)
+            {
+                EditorGUI.LabelField(position, label.text, "EnumArray not initialized");
+                return;
+            }
+
+            var enumType = fieldInfo.FieldType.GetGenericArguments()[0];
+            var enumNames = Enum.GetNames(enumType);
+
+            if (valuesProperty.arraySize != enumNames.Length)
+                valuesProperty.arraySize = enumNames.Length;
+
+            EditorGUI.BeginProperty(position, label, property);
+
+            var foldout = property.isExpanded;
+            var foldoutRect = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+            property.isExpanded = EditorGUI.Foldout(foldoutRect, foldout, label);
+
+            if (property.isExpanded)
+            {
+                EditorGUI.indentLevel++;
+                var yPos = position.y + EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+
+                for (var i = 0; i < valuesProperty.arraySize && i < enumNames.Length; i++)
+                {
+                    var elementProperty = valuesProperty.GetArrayElementAtIndex(i);
+                    var elementHeight = EditorGUI.GetPropertyHeight(elementProperty, true);
+                    var elementRect = new Rect(position.x, yPos, position.width, elementHeight);
+
+                    EditorGUI.PropertyField(elementRect, elementProperty, new GUIContent(enumNames[i]), true);
+
+                    yPos += elementHeight + EditorGUIUtility.standardVerticalSpacing;
+                }
+
+                EditorGUI.indentLevel--;
+            }
+
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var valuesProperty = property.FindPropertyRelative("_values");
+            if (valuesProperty == null || property.isExpanded is false)
+                return EditorGUIUtility.singleLineHeight;
+
+            var enumType = fieldInfo.FieldType.GetGenericArguments()[0];
+            var enumNames = Enum.GetNames(enumType);
+
+            var height = EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+
+            for (var i = 0; i < valuesProperty.arraySize && i < enumNames.Length; i++)
+            {
+                var elementProperty = valuesProperty.GetArrayElementAtIndex(i);
+                height += EditorGUI.GetPropertyHeight(elementProperty, true) + EditorGUIUtility.standardVerticalSpacing;
+            }
+
+            return height;
+        }
+    }
+}
---
+++ Runtime/CustomTypes/EnumArray.cs
@@ -0,0 +1,50 @@
+﻿// EnumArray.cs
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using CustomUtils.Unsafe.CustomUtils.Unsafe;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.CustomTypes
+{
+    [Serializable]
+    public struct EnumArray<TEnum, TValue> : IEnumerable<TValue>
+        where TEnum : unmanaged, Enum
+    {
+        [SerializeField] private TValue[] _values;
+
+        public TValue[] Values
+        {
+            get
+            {
+                if (_values != null)
+                    return _values;
+
+                var enumValues = (TEnum[])Enum.GetValues(typeof(TEnum));
+                _values = new TValue[enumValues.Length];
+
+                return _values;
+            }
+        }
+
+        public EnumArray(TValue defaultValue)
+        {
+            var enumValues = (TEnum[])Enum.GetValues(typeof(TEnum));
+            _values = new TValue[enumValues.Length];
+            for (var i = 0; i < _values.Length; i++)
+                _values[i] = defaultValue;
+        }
+
+        public TValue this[TEnum key]
+        {
+            get => Values[UnsafeEnumConverter<TEnum>.ToInt32(key)];
+            set => Values[UnsafeEnumConverter<TEnum>.ToInt32(key)] = value;
+        }
+
+        public int Length => Values.Length;
+
+        public IEnumerator<TValue> GetEnumerator() => Values.AsEnumerable().GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
