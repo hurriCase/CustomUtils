410524203cf5ae6ceda689291368bd602a081a6d
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-07-12 15:08:10 +0300

    [feat] SetAlpha graphic extension

---
+++ Runtime/Extensions/GraphicExtensions.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using UnityEngine.UI;
+
+namespace CustomUtils.Runtime.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for UnityEngine.UI.Graphic.
+    /// </summary>
+    [UsedImplicitly]
+    public static class GraphicExtensions
+    {
+        /// <summary>
+        /// Sets the alpha value of the graphic's color.
+        /// </summary>
+        /// <param name="graphic">The graphic to modify.</param>
+        /// <param name="alpha">The alpha value to set, where 0 is fully transparent and 1 is fully opaque.</param>
+        [UsedImplicitly]
+        public static void SetAlpha(this Graphic graphic, float alpha)
+        {
+            var color = graphic.color;
+            color.a = alpha;
+            graphic.color = color;
+        }
+    }
+}
