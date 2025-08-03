cddbe40a9e5545df03f5320c22ffc4af2a7a9214
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-08-03 16:50:50 +0300

    [refactor] interface resolution instead of singleton pattern for AudioHandlerGeneric

--- Runtime/Audio/AudioHandlerGeneric.cs
+++ Runtime/Audio/AudioHandlerGeneric.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
-using CustomUtils.Runtime.CustomTypes.Singletons;
 using CustomUtils.Runtime.Storage;
 using CustomUtils.Unsafe.CustomUtils.Unsafe;
 using Cysharp.Threading.Tasks;
-using JetBrains.Annotations;
 using PrimeTween;
 using R3;
 using UnityEngine;
@@ -12,52 +10,19 @@ using ZLinq;
 
 namespace CustomUtils.Runtime.Audio
 {
-    /// <inheritdoc />
-    /// <summary>
-    /// Base class for handling audio playback including music and sound effects
-    /// </summary>
-    /// <typeparam name="TMusicType">Enum type for music categories</typeparam>
-    /// <typeparam name="TSoundType">Enum type for sound effect categories</typeparam>
-    [UsedImplicitly]
-    public abstract class AudioHandlerGeneric<TMusicType, TSoundType> : MonoBehaviour
+    public abstract class AudioHandlerGeneric<TMusicType, TSoundType> : MonoBehaviour,
+        IAudioHandlerGeneric<TMusicType, TSoundType>
         where TMusicType : unmanaged, Enum
         where TSoundType : unmanaged, Enum
     {
-        [UsedImplicitly] public static AudioHandlerGeneric<TMusicType, TSoundType> Instance { get; private set; }
+        [SerializeField] protected AudioDatabaseGeneric<TMusicType, TSoundType> audioDatabaseGeneric;
+        [SerializeField] protected AudioSource soundSourcePrefab;
+        [SerializeField] protected AudioSource musicSource;
+        [SerializeField] protected AudioSource oneShotSource;
+        [SerializeField] protected int defaultSoundPoolCount = 3;
 
-#if UNITY_EDITOR
-        static AudioHandlerGeneric()
-        {
-            SingletonResetter.RegisterResetAction(() =>
-            {
-                Instance = null;
-            });
-        }
-#endif
-        protected virtual void Awake()
-        {
-            if (!Instance)
-            {
-                Instance = this;
-
-                DontDestroyOnLoad(gameObject);
-                return;
-            }
-
-            if (Instance != this)
-                Destroy(gameObject);
-        }
-
-        [SerializeField] protected AudioDatabaseGeneric<TMusicType, TSoundType> _audioDatabaseGeneric;
-        [SerializeField] protected AudioSource _soundSourcePrefab;
-        [SerializeField] protected AudioSource _musicSource;
-        [SerializeField] protected AudioSource _oneShotSource;
-        [SerializeField] protected int _defaultSoundPoolCount = 3;
-
-        [UsedImplicitly]
         public virtual PersistentReactiveProperty<float> MusicVolume { get; } = new();
 
-        [UsedImplicitly]
         public virtual PersistentReactiveProperty<float> SoundVolume { get; } = new();
 
         private readonly Dictionary<int, float> _lastPlayedTimes = new();
@@ -69,19 +34,15 @@ namespace CustomUtils.Runtime.Audio
         private const string MusicVolumeKey = "MusicVolumeKey";
         private const string SoundVolumeKey = "SoundVolumeKey";
 
-        /// <summary>
-        /// Initializes the audio handler with pooled audio sources and volume subscriptions
-        /// </summary>
-        [UsedImplicitly]
         public virtual async UniTask InitAsync(float defaultMusicVolume = 1f, float defaultSoundVolume = 1f)
         {
             await MusicVolume.InitAsync(MusicVolumeKey, destroyCancellationToken, defaultMusicVolume);
             await SoundVolume.InitAsync(SoundVolumeKey, destroyCancellationToken, defaultSoundVolume);
 
             _soundPool = new PoolHandler<AudioSource>();
-            _soundPool.Init(_soundSourcePrefab, _defaultSoundPoolCount, _defaultSoundPoolCount * 5);
+            _soundPool.Init(soundSourcePrefab, defaultSoundPoolCount, defaultSoundPoolCount * 5);
 
-            _audioDatabaseGeneric.Init();
+            audioDatabaseGeneric.Init();
 
             var soundDisposable = SoundVolume
                 .Subscribe(this, (volume, handler) => handler.OnSoundVolumeChanged(volume));
@@ -92,17 +53,9 @@ namespace CustomUtils.Runtime.Audio
             _disposable = Disposable.Combine(soundDisposable, musicDisposable);
         }
 
-        /// <summary>
-        /// Plays a sound with the specified parameters
-        /// </summary>
-        /// <param name="soundType">Type of sound to play</param>
-        /// <param name="volumeModifier">Volume multiplier</param>
-        /// <param name="pitchModifier">Pitch multiplier</param>
-        /// <returns>AudioSource playing the sound, or null if failed</returns>
-        [UsedImplicitly]
         public virtual AudioSource PlaySound(TSoundType soundType, float volumeModifier = 1, float pitchModifier = 1)
         {
-            var soundData = _audioDatabaseGeneric.GetSoundContainer(soundType);
+            var soundData = audioDatabaseGeneric.GetSoundContainer(soundType);
 
             if (soundData?.AudioData == null || !soundData.AudioData?.AudioClip)
                 return null;
@@ -139,11 +92,6 @@ namespace CustomUtils.Runtime.Audio
             return soundSource;
         }
 
-        /// <summary>
-        /// Stops all instances of the specified sound type
-        /// </summary>
-        /// <param name="soundType">Type of sound to stop</param>
-        [UsedImplicitly]
         public virtual void StopSound(TSoundType soundType)
         {
             var soundId = UnsafeEnumConverter<TSoundType>.ToInt32(soundType);
@@ -165,54 +113,35 @@ namespace CustomUtils.Runtime.Audio
                 _sortedAliveAudioData.Remove(key);
         }
 
-        /// <summary>
-        /// Plays a one-shot sound that doesn't need to be tracked or stopped
-        /// </summary>
-        /// <param name="soundType">Type of sound to play</param>
-        /// <param name="volumeModifier">Volume multiplier</param>
-        /// <param name="pitchModifier">Pitch multiplier</param>
-        [UsedImplicitly]
         public virtual void PlayOneShotSound(TSoundType soundType, float volumeModifier = 1, float pitchModifier = 1)
         {
-            var soundData = _audioDatabaseGeneric.GetSoundContainer(soundType);
+            var soundData = audioDatabaseGeneric.GetSoundContainer(soundType);
 
             if (soundData?.AudioData == null || !soundData.AudioData?.AudioClip)
                 return;
 
-            _oneShotSource.pitch = pitchModifier * soundData.AudioData.RandomPitch;
-            _oneShotSource.volume = SoundVolume.Value * volumeModifier * soundData.AudioData.RandomVolume;
-            _oneShotSource.PlayOneShot(soundData.AudioData.AudioClip);
+            oneShotSource.pitch = pitchModifier * soundData.AudioData.RandomPitch;
+            oneShotSource.volume = SoundVolume.Value * volumeModifier * soundData.AudioData.RandomVolume;
+            oneShotSource.PlayOneShot(soundData.AudioData.AudioClip);
         }
 
-        /// <summary>
-        /// Plays music of the specified type
-        /// </summary>
-        /// <param name="musicType">Type of music to play</param>
-        /// <returns>AudioSource playing the music, or null if failed</returns>
-        [UsedImplicitly]
         public virtual AudioSource PlayMusic(TMusicType musicType)
         {
-            var musicData = _audioDatabaseGeneric.GetMusicContainer(musicType);
+            var musicData = audioDatabaseGeneric.GetMusicContainer(musicType);
             return musicData?.AudioData == null ? null : PlayMusic(musicData.AudioData);
         }
 
-        /// <summary>
-        /// Plays music from the specified audio data
-        /// </summary>
-        /// <param name="data">Audio data to play</param>
-        /// <returns>AudioSource playing the music, or null if failed</returns>
-        [UsedImplicitly]
         public virtual AudioSource PlayMusic(AudioData data)
         {
             if (data == null || !data.AudioClip)
                 return null;
 
-            _musicSource.clip = data.AudioClip;
-            _musicSource.pitch = data.RandomPitch;
-            _musicSource.volume = data.RandomVolume * MusicVolume.Value;
-            _musicSource.Play();
+            musicSource.clip = data.AudioClip;
+            musicSource.pitch = data.RandomPitch;
+            musicSource.volume = data.RandomVolume * MusicVolume.Value;
+            musicSource.Play();
 
-            return _musicSource;
+            return musicSource;
         }
 
         /// <summary>
@@ -231,7 +160,7 @@ namespace CustomUtils.Runtime.Audio
         /// <param name="musicVolume">New music volume level</param>
         protected virtual void OnMusicVolumeChanged(float musicVolume)
         {
-            _musicSource.volume *= musicVolume;
+            musicSource.volume *= musicVolume;
         }
 
         /// <summary>
---
+++ Runtime/Audio/IAudioHandlerGeneric.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using CustomUtils.Runtime.Storage;
+using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.Audio
+{
+    /// <summary>
+    /// Base class for handling audio playback including music and sound effects
+    /// </summary>
+    /// <typeparam name="TMusicType">Enum type for music categories</typeparam>
+    /// <typeparam name="TSoundType">Enum type for sound effect categories</typeparam>
+    [UsedImplicitly]
+    public interface IAudioHandlerGeneric<in TMusicType, in TSoundType>
+        where TMusicType : unmanaged, Enum
+        where TSoundType : unmanaged, Enum
+    {
+        /// <summary>
+        /// Gets the reactive property for music volume control.
+        /// Value range is typically 0.0 to 1.0, where 0 is muted and 1 is full volume.
+        /// Changes to this property will automatically persist to storage and update all active music sources.
+        /// </summary>
+        [UsedImplicitly]
+        PersistentReactiveProperty<float> MusicVolume { get; }
+
+        /// <summary>
+        /// Gets the reactive property for sound effects volume control.
+        /// Value range is typically 0.0 to 1.0, where 0 is muted and 1 is full volume.
+        /// Changes to this property will automatically persist to storage and update all active sound sources.
+        /// </summary>
+        [UsedImplicitly]
+        PersistentReactiveProperty<float> SoundVolume { get; }
+
+        /// <summary>
+        /// Initializes the audio handler with pooled audio sources and volume subscriptions
+        /// </summary>
+        [UsedImplicitly]
+        UniTask InitAsync(float defaultMusicVolume = 1f, float defaultSoundVolume = 1f);
+
+        /// <summary>
+        /// Plays a sound with the specified parameters
+        /// </summary>
+        /// <param name="soundType">Type of sound to play</param>
+        /// <param name="volumeModifier">Volume multiplier</param>
+        /// <param name="pitchModifier">Pitch multiplier</param>
+        /// <returns>AudioSource playing the sound, or null if failed</returns>
+        [UsedImplicitly]
+        AudioSource PlaySound(TSoundType soundType, float volumeModifier = 1, float pitchModifier = 1);
+
+        /// <summary>
+        /// Stops all instances of the specified sound type
+        /// </summary>
+        /// <param name="soundType">Type of sound to stop</param>
+        [UsedImplicitly]
+        void StopSound(TSoundType soundType);
+
+        /// <summary>
+        /// Plays a one-shot sound that doesn't need to be tracked or stopped
+        /// </summary>
+        /// <param name="soundType">Type of sound to play</param>
+        /// <param name="volumeModifier">Volume multiplier</param>
+        /// <param name="pitchModifier">Pitch multiplier</param>
+        [UsedImplicitly]
+        void PlayOneShotSound(TSoundType soundType, float volumeModifier = 1, float pitchModifier = 1);
+
+        /// <summary>
+        /// Plays music of the specified type
+        /// </summary>
+        /// <param name="musicType">Type of music to play</param>
+        /// <returns>AudioSource playing the music, or null if failed</returns>
+        [UsedImplicitly]
+        AudioSource PlayMusic(TMusicType musicType);
+
+        /// <summary>
+        /// Plays music from the specified audio data
+        /// </summary>
+        /// <param name="data">Audio data to play</param>
+        /// <returns>AudioSource playing the music, or null if failed</returns>
+        [UsedImplicitly]
+        AudioSource PlayMusic(AudioData data);
+    }
+}
