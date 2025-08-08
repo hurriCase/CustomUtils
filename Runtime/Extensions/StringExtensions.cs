c7fd89e321ec2936c526be96e23d79fb940b5a5f
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-08-08 06:39:17 +0300

    [feat] GetLocalization overload with language support for StringExtensions.cs

--- Runtime/Extensions/StringExtensions.cs
+++ Runtime/Extensions/StringExtensions.cs
@@ -2,6 +2,7 @@
 using System.Text.RegularExpressions;
 using CustomUtils.Runtime.Localization;
 using JetBrains.Annotations;
+using UnityEngine;
 
 namespace CustomUtils.Runtime.Extensions
 {
@@ -52,5 +53,15 @@ namespace CustomUtils.Runtime.Extensions
         /// <returns>The localized string corresponding to the key, or the key itself if no localization is found.</returns>
         [UsedImplicitly]
         public static string GetLocalization(this string key) => LocalizationController.Localize(key);
+
+        /// <summary>
+        /// Gets the localized string for the specified localization key.
+        /// </summary>
+        /// <param name="key">The localization key to retrieve the translated text for.</param>
+        /// <param name="language">The specific language to use for localization.</param>
+        /// <returns>The localized string corresponding to the key, or the key itself if no localization is found.</returns>
+        [UsedImplicitly]
+        public static string GetLocalization(this string key, SystemLanguage language)
+            => LocalizationController.Localize(key, language);
     }
 }
