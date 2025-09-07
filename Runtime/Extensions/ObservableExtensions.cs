3d1a8547692069a93384fb4c24070173953ed07b
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-07 07:57:22 +0300

    [feat] Where extension in ObservableExtensions.cs

--- Runtime/Extensions/ObservableExtensions.cs
+++ Runtime/Extensions/ObservableExtensions.cs
@@ -95,5 +95,24 @@ namespace CustomUtils.Runtime.Extensions
                     static (value, state) => state.onNext(value, state.self, state.tuple))
                 .RegisterTo(self.GetCancellationTokenOnDestroy());
         }
+
+        /// <summary>
+        /// Filters observable based on predicate that only uses the MonoBehaviour instance.
+        /// </summary>
+        /// <typeparam name="TSelf">MonoBehaviour type.</typeparam>
+        /// <typeparam name="T">Observable value type.</typeparam>
+        /// <param name="observable">Observable to filter.</param>
+        /// <param name="self">MonoBehaviour instance for predicate.</param>
+        /// <param name="predicate">Predicate function that takes only MonoBehaviour instance.</param>
+        /// <returns>Filtered observable.</returns>
+        [UsedImplicitly]
+        public static Observable<T> Where<TSelf, T>(
+            this Observable<T> observable,
+            TSelf self,
+            Func<TSelf, bool> predicate)
+            where TSelf : MonoBehaviour
+        {
+            return observable.Where((self, predicate), static (_, tuple) => tuple.predicate(tuple.self));
+        }
     }
 }
