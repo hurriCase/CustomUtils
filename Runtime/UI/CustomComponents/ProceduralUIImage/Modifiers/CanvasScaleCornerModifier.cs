154bc1759714d3fdbfbfa41f64681cbf652cf335
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-12 06:31:46 +0300

    [feat] CanvasScaleCornerModifier

---
+++ Runtime/UI/CustomComponents/ProceduralUIImage/Modifiers/CanvasScaleCornerModifier.cs
@@ -0,0 +1,27 @@
+﻿using CustomUtils.Runtime.UI.CustomComponents.ProceduralUIImage.Attributes;
+using CustomUtils.Runtime.UI.CustomComponents.ProceduralUIImage.Modifiers.Base;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.UI.CustomComponents.ProceduralUIImage.Modifiers
+{
+    [ModifierID("Canvas Scale")]
+    public sealed class CanvasScaleCornerModifier : ModifierBase
+    {
+        [field: SerializeField] public float Radius { get; private set; }
+
+        [SerializeField] private Canvas _canvasScaleReference;
+
+        public override Vector4 CalculateRadius(Rect imageRect)
+        {
+            var minSide = Mathf.Min(imageRect.width, imageRect.height);
+
+            var scaleFactor = _canvasScaleReference.scaleFactor;
+            var scaledRadius = Radius * scaleFactor;
+
+            var maxAllowedRadius = minSide * 0.5f;
+            var actualRadius = Mathf.Min(scaledRadius, maxAllowedRadius);
+
+            return new Vector4(actualRadius, actualRadius, actualRadius, actualRadius);
+        }
+    }
+}
