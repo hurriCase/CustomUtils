4c12e0bc79d12fee1340a9d9b503b46794cdec45
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-13 06:55:24 +0300

    [refactor] invert HideIf into ShowIf attribute

--- Editor/AttributeDrawers/HideIfPropertyDrawer.cs
+++ Editor/AttributeDrawers/HideIfPropertyDrawer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using CustomUtils.Editor.CustomEditorUtilities;
 using CustomUtils.Editor.Extensions;
 using CustomUtils.Runtime.Attributes;
@@ -8,15 +7,15 @@ using UnityEngine;
 
 namespace CustomUtils.Editor.AttributeDrawers
 {
-    [CustomPropertyDrawer(typeof(HideIfAttribute))]
-    public class HideIfPropertyDrawer : PropertyDrawer
+    [CustomPropertyDrawer(typeof(ShowIfAttribute))]
+    internal sealed class ShowIfPropertyDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            if (attribute is not HideIfAttribute hideIfAttribute)
+            if (attribute is not ShowIfAttribute showIfAttribute)
                 return;
 
-            var validationResult = ValidateConditionalField(hideIfAttribute, property);
+            var validationResult = ValidateConditionalField(showIfAttribute, property);
 
             if (validationResult.IsValid is false)
             {
@@ -24,46 +23,46 @@ namespace CustomUtils.Editor.AttributeDrawers
                 return;
             }
 
-            if (IsHideProperty(hideIfAttribute, property) is false)
+            if (ShouldShowProperty(showIfAttribute, property))
                 EditorGUI.PropertyField(position, property, label, true);
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            if (attribute is not HideIfAttribute hideIfAttribute)
+            if (attribute is not ShowIfAttribute showIfAttribute)
                 return EditorGUI.GetPropertyHeight(property, label);
 
-            var validationResult = ValidateConditionalField(hideIfAttribute, property);
+            var validationResult = ValidateConditionalField(showIfAttribute, property);
 
             if (validationResult.IsValid is false)
                 return EditorGUIUtility.singleLineHeight * 1.5f + EditorGUIUtility.standardVerticalSpacing;
 
-            if (IsHideProperty(hideIfAttribute, property))
+            if (ShouldShowProperty(showIfAttribute, property) is false)
                 return -EditorGUIUtility.standardVerticalSpacing;
 
             return EditorGUI.GetPropertyHeight(property, label);
         }
 
-        private bool IsHideProperty(HideIfAttribute hideIfAttribute, SerializedProperty property)
+        private bool ShouldShowProperty(ShowIfAttribute showIfAttribute, SerializedProperty property)
         {
-            if (TryGetSourceProperty(hideIfAttribute, property, out var sourceProperty) is false)
+            if (TryGetSourceProperty(showIfAttribute, property, out var sourceProperty) is false)
                 return false;
 
-            return hideIfAttribute.HideType switch
+            return showIfAttribute.ShowType switch
             {
-                HideType.True => sourceProperty.boolValue,
-                HideType.False => sourceProperty.boolValue is false,
-                _ => false
+                ShowType.True => sourceProperty.boolValue,
+                ShowType.False => sourceProperty.boolValue is false,
+                _ => true
             };
         }
 
-        private Result ValidateConditionalField(HideIfAttribute hideIfAttribute, SerializedProperty property)
+        private Result ValidateConditionalField(ShowIfAttribute showIfAttribute, SerializedProperty property)
         {
-            if (TryGetSourceProperty(hideIfAttribute, property, out var sourceProperty))
+            if (TryGetSourceProperty(showIfAttribute, property, out var sourceProperty))
             {
                 if (sourceProperty.propertyType != SerializedPropertyType.Boolean)
                     return Result.Invalid(
-                        $"HideIf field '{hideIfAttribute.ConditionalSourceField}' must be a boolean, " +
+                        $"ShowIf field '{showIfAttribute.ConditionalSourceField}' must be a boolean, " +
                         $"but found {sourceProperty.propertyType}");
 
                 return Result.Valid();
@@ -72,31 +71,31 @@ namespace CustomUtils.Editor.AttributeDrawers
             var targetObject = property.serializedObject.targetObject;
             var targetType = targetObject.GetType();
 
-            var info = targetType.GetField(hideIfAttribute.ConditionalSourceField,
+            var info = targetType.GetField(showIfAttribute.ConditionalSourceField,
                 BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
 
-            var propertyInfo = targetType.GetProperty(hideIfAttribute.ConditionalSourceField,
+            var propertyInfo = targetType.GetProperty(showIfAttribute.ConditionalSourceField,
                 BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
 
             if (info == null)
                 return Result.Invalid(propertyInfo != null
-                    ? $"HideIf references property '{hideIfAttribute.ConditionalSourceField}' " +
+                    ? $"ShowIf references property '{showIfAttribute.ConditionalSourceField}' " +
                       "but properties are not serialized. Use a [SerializeField] field instead."
-                    : $"HideIf field '{hideIfAttribute.ConditionalSourceField}' not found in class {targetType.Name}.");
+                    : $"ShowIf field '{showIfAttribute.ConditionalSourceField}' not found in class {targetType.Name}.");
 
-            var existError = $"HideIf field '{hideIfAttribute.ConditionalSourceField}' exists but is not";
+            var existError = $"ShowIf field '{showIfAttribute.ConditionalSourceField}' exists but is not";
             return Result.Invalid(info.FieldType == typeof(bool)
-                ? existError + "serialized. Add [SerializeField] to make it work."
-                : existError + " a boolean ({info.FieldType.Name}).");
+                ? existError + " serialized. Add [SerializeField] to make it work."
+                : existError + $" a boolean ({info.FieldType.Name}).");
         }
 
         private bool TryGetSourceProperty(
-            HideIfAttribute hideIfAttribute,
+            ShowIfAttribute showIfAttribute,
             SerializedProperty property,
             out SerializedProperty sourceProperty)
         {
             var serializedObject = property.serializedObject;
-            var sourceFieldName = hideIfAttribute.ConditionalSourceField;
+            var sourceFieldName = showIfAttribute.ConditionalSourceField;
             sourceProperty = serializedObject.FindProperty(sourceFieldName)
                              ?? serializedObject.FindField(sourceFieldName);
 
--- Runtime/Attributes/HideIfAttribute.cs
+++ Runtime/Attributes/HideIfAttribute.cs
@@ -6,72 +6,65 @@ namespace CustomUtils.Runtime.Attributes
 {
     /// <inheritdoc />
     /// <summary>
-    /// Attribute that conditionally hides a field or property in the Unity Inspector based on the value of another field.
+    /// Attribute that conditionally shows a field or property in the Unity Inspector based on the value of another field.
     /// Can be applied to any serialized field or property to create dynamic Inspector behavior.
     /// </summary>
-    /// <remarks>
-    /// This attribute allows you to show or hide fields based on boolean conditions from other fields,
-    /// creating more dynamic and user-friendly Inspector layouts. The conditional field must be a boolean
-    /// field within the same class.
-    /// </remarks>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
     [UsedImplicitly]
-    public sealed class HideIfAttribute : PropertyAttribute
+    public sealed class ShowIfAttribute : PropertyAttribute
     {
         /// <summary>
         /// Gets the name of the field that controls the visibility of the decorated field.
         /// </summary>
-        /// <remarks>
-        /// This field must be a boolean field within the same class as the decorated field.
-        /// </remarks>
         [UsedImplicitly]
         public string ConditionalSourceField { get; private set; }
 
         /// <summary>
-        /// Gets the hide type that determines when the field should be hidden.
+        /// Gets the show type that determines when the field should be shown.
         /// </summary>
-        /// <value>
-        /// A <see cref="HideType"/> value that specifies the condition for hiding the field.
-        /// </value>
         [UsedImplicitly]
-        public HideType HideType { get; private set; }
+        public ShowType ShowType { get; private set; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="HideIfAttribute"/> class.
+        /// Initializes a new instance of the ShowIfAttribute class.
         /// </summary>
         /// <param name="conditionalSourceField">
         /// The name of the boolean field that controls the visibility of the decorated field.
         /// This field must exist within the same class.
         /// </param>
-        /// <param name="hideType">
-        /// Specifies when to hide the field. Default is <see cref="HideType.False"/>.
+        /// <param name="showType">
+        /// Specifies when to show the field. Default is ShowType.True.
         /// </param>
         [UsedImplicitly]
-        public HideIfAttribute([NotNull] string conditionalSourceField, HideType hideType = HideType.False)
+        public ShowIfAttribute(string conditionalSourceField, ShowType showType = ShowType.True)
         {
             ConditionalSourceField = conditionalSourceField;
-            HideType = hideType;
+            ShowType = showType;
         }
     }
 
     /// <summary>
-    /// Specifies the condition under which a field should be hidden in the Unity Inspector.
+    /// Specifies the condition under which a field should be shown in the Unity Inspector.
     /// </summary>
-    public enum HideType
+    [UsedImplicitly]
+    public enum ShowType
     {
         /// <summary>
-        /// No hiding behavior (field is always shown).
+        /// No showing behavior (field visibility unchanged).
         /// </summary>
+        [UsedImplicitly]
         None = 0,
 
         /// <summary>
-        /// Hide the field when the conditional source field is true.
+        /// Show the field when the conditional source field is true.
         /// </summary>
+        [UsedImplicitly]
         True = 1,
 
         /// <summary>
-        /// Hide the field when the conditional source field is false.
+        /// Show the field when the conditional source field is false.
         /// </summary>
+        [UsedImplicitly]
         False = 2
     }
 }
