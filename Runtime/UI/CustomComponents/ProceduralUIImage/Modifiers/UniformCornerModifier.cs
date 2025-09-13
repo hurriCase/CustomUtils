7b88be91bbddd265b2cc1d4c95815e92ceccaaee
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-13 06:29:46 +0300

    [fix] missing changes with UniformCornerModifier.cs

--- Runtime/UI/CustomComponents/ProceduralUIImage/Modifiers/UniformCornerModifier.cs
+++ Runtime/UI/CustomComponents/ProceduralUIImage/Modifiers/UniformCornerModifier.cs
@@ -9,11 +9,12 @@ namespace CustomUtils.Runtime.UI.CustomComponents.ProceduralUIImage.Modifiers
     {
         [field: SerializeField] public float Radius { get; private set; }
 
-        protected override Vector4 OnCalculateRadius(Rect imageRect)
+        public override Vector4 CalculateRadius(Rect imageRect)
         {
             var minSide = Mathf.Min(imageRect.width, imageRect.height);
             var maxAllowedRadius = minSide * 0.5f;
-            var scaledRadius = Radius * RootCanvas.scaleFactor;
+            var scaleFactor = rootCanvasProvider ? rootCanvasProvider.ScaleFactor : 1f;
+            var scaledRadius = Radius * scaleFactor;
             var actualRadius = Mathf.Min(scaledRadius, maxAllowedRadius);
 
             return new Vector4(actualRadius, actualRadius, actualRadius, actualRadius);
