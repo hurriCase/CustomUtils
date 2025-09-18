ebe3ab8c9707d399a0715c8d95c2451c34516380
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-18 07:35:02 +0300

    [feat] AnimationCurveExtensions

---
+++ Runtime/Extensions/AnimationCurveExtensions.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.Extensions
+{
+    internal static class AnimationCurveExtensions
+    {
+        /// <summary>
+        /// Gets the value of the animation curve at its last keyframe's time.
+        /// This is useful for determining the final/maximum value that the curve reaches.
+        /// </summary>
+        /// <param name="animationCurve">The animation curve to evaluate.</param>
+        /// <returns>The curve value at the last keyframe's time position.</returns>
+        /// <exception cref="System.IndexOutOfRangeException">Thrown if the curve has no keyframes.</exception>
+        [UsedImplicitly]
+        internal static float GetLastValue(this AnimationCurve animationCurve)
+            => animationCurve.Evaluate(animationCurve.keys[^1].time);
+    }
+}
