da9c888e9680bdbabf5a55abbf38f8d1b67e6d37
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-22 07:42:40 +0300

    [feat] Self auto component attribute

---
+++ Runtime/Attributes/SelfAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace CustomUtils.Runtime.Attributes
+{
+    [UsedImplicitly]
+    [AttributeUsage(AttributeTargets.Field)]
+    public sealed class SelfAttribute : PropertyAttribute { }
+}
