4b8628ddeb7c2cccb6bba17e46e01286e6c8115a
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-23 07:06:36 +0300

    [refactor] ColorData.cs simplification

--- Runtime/UI/Theme/ThemeMapping/ColorData.cs
+++ Runtime/UI/Theme/ThemeMapping/ColorData.cs
@@ -5,30 +5,9 @@ using UnityEngine;
 namespace CustomUtils.Runtime.UI.Theme.ThemeMapping
 {
     [Serializable]
-    public struct ColorData : IEquatable<ColorData>
+    public struct ColorData
     {
-        [field: SerializeField] public ColorType ColorType { get; set; }
-        [field: SerializeField] public string ColorName { get; set; }
-
-        public ColorData(ColorType colorType = ColorType.Solid, string colorName = null)
-        {
-            ColorType = colorType;
-            ColorName = colorName ?? string.Empty;
-        }
-
-        public bool Equals(ColorData other) =>
-            ColorType == other.ColorType && ColorName == other.ColorName;
-
-        public override bool Equals(object obj) =>
-            obj is ColorData other && Equals(other);
-
-        public override int GetHashCode() =>
-            HashCode.Combine((int)ColorType, ColorName);
-
-        public static bool operator ==(ColorData left, ColorData right) =>
-            left.Equals(right);
-
-        public static bool operator !=(ColorData left, ColorData right) =>
-            left.Equals(right) is false;
+        [field: SerializeField] public ColorType ColorType { get; private set; }
+        [field: SerializeField] public string ColorName { get; private set; }
     }
 }
