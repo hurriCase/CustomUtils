7971fa5a83b40cdbecefa57b8aaea6d7ce4bf0c5
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-24 12:44:37 +0300

    [refactor] move BaseFieldExtensionsEditor.cs into SerializedExtensionsEditor.cs

--- Editor/Scripts/Extensions/SerializedExtensionsEditor.cs
+++ Editor/Scripts/Extensions/SerializedExtensionsEditor.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Reflection;
 using CustomUtils.Runtime.Extensions;
 using JetBrains.Annotations;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace CustomUtils.Editor.Scripts.Extensions
 {
@@ -12,6 +15,53 @@ namespace CustomUtils.Editor.Scripts.Extensions
     [UsedImplicitly]
     public static class SerializedExtensionsEditor
     {
+        private const string ValuePropertyName = "value";
+
+        /// <summary>
+        /// Retrieves the reactive serialized property associated with the specified property name
+        /// within the given serialized object. The returned property represents the "value" field
+        /// of the reactive property.
+        /// </summary>
+        /// <param name="serializedObject">The serialized object containing the reactive property.</param>
+        /// <param name="propertyName">The name of the reactive property to access.</param>
+        /// <returns>The serialized property representing the "value" field of the specified reactive property.</returns>
+        [UsedImplicitly]
+        public static SerializedProperty GetReactiveSerializedProperty(
+            this SerializedObject serializedObject,
+            string propertyName)
+        {
+            var reactivePropertyField = serializedObject.FindField(propertyName);
+            var valueProperty = reactivePropertyField.FindPropertyRelative(ValuePropertyName);
+            return valueProperty;
+        }
+
+        /// <summary>
+        /// Binds a reactive property from a serialized object to a UI element,
+        /// enabling two-way binding and change notifications.
+        /// </summary>
+        /// <typeparam name="T">The type of the property being bound.</typeparam>
+        /// <param name="element">The UI element to bind the property to.</param>
+        /// <param name="serializedObject">The serialized object containing the property to bind.</param>
+        /// <param name="propertyName">The name of the reactive property to bind.</param>
+        [UsedImplicitly]
+        public static void BindReactiveProperty<T>(
+            this SerializedObject serializedObject,
+            BaseField<T> element,
+            string propertyName)
+        {
+            var reactivePropertyField = serializedObject.FindField(propertyName);
+            var valueProperty = reactivePropertyField.FindPropertyRelative(ValuePropertyName);
+
+            element.BindProperty(valueProperty);
+
+            element.RegisterValueChangedCallback(_ =>
+            {
+                serializedObject.ApplyModifiedProperties();
+
+                TriggerReactivePropertyNotification(serializedObject, reactivePropertyField);
+            });
+        }
+
         /// <summary>
         /// Finds a serialized property using its backing field name format.
         /// </summary>
@@ -105,6 +155,54 @@ namespace CustomUtils.Editor.Scripts.Extensions
             var targetProperty = parentProperty.FindFieldRelative(propertyName);
 
             return targetProperty != null ? (T)(object)targetProperty.enumValueIndex : default;
+        }
+
+        private static void TriggerReactivePropertyNotification(
+            SerializedObject serializedObject,
+            SerializedProperty reactivePropertyField)
+        {
+            try
+            {
+                var targetObject = serializedObject.targetObject;
+                var reactivePropertyInstance =
+                    GetReactivePropertyInstance(targetObject, reactivePropertyField.propertyPath);
+
+                if (reactivePropertyInstance == null)
+                    return;
+
+                var forceNotifyMethod = reactivePropertyInstance.GetType().GetMethod("ForceNotify",
+                    BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+                forceNotifyMethod?.Invoke(reactivePropertyInstance, Array.Empty<object>());
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("[NotifyValueChangedExtensionsEditor::TriggerReactivePropertyNotification] '" +
+                               $"Failed to trigger reactive property notification: {ex.Message}");
+
+                Debug.LogException(ex);
+            }
+        }
+
+        private static object GetReactivePropertyInstance(object targetObject, string propertyPath)
+        {
+            var pathParts = propertyPath.Split('.');
+            var currentObject = targetObject;
+
+            foreach (var pathPart in pathParts)
+            {
+                if (currentObject == null)
+                    break;
+
+                var fieldInfo = currentObject.GetType().GetFieldInfo(pathPart);
+
+                if (fieldInfo != null)
+                    currentObject = fieldInfo.GetValue(currentObject);
+                else
+                    return null;
+            }
+
+            return currentObject;
         }
     }
 }
