8e2e2556d8c9cdc7842c2eee428ee9ad163dea37
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-25 12:05:03 +0300

    [refactor] GetOrAddComponent in GameObjectExtensions

--- Runtime/Extensions/GameObjectExtensions.cs
+++ Runtime/Extensions/GameObjectExtensions.cs
@@ -17,7 +17,10 @@ namespace CustomUtils.Runtime.Extensions
         /// <param name="gameObject">The gameObject is searched for the specified type.</param>
         /// <returns>The existing component of type T, or the newly added component if it did not exist.</returns>
         [UsedImplicitly]
-        public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component =>
-            gameObject.GetComponent<T>() ? gameObject.GetComponent<T>() : gameObject.AddComponent<T>();
+        public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+        {
+            var component = gameObject.GetComponent<T>();
+            return component ? component : gameObject.AddComponent<T>();
+        }
     }
 }
