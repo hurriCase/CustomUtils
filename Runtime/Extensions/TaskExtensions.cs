88850fb7f6ea6980158683eb6612485833c4b94d
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-27 15:47:06 +0300

    [feat] TaskExtensions

---
+++ Runtime/Extensions/TaskExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace CustomUtils.Runtime.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="Task"/>.
+    /// </summary>
+    [UsedImplicitly]
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Converts a Task to UniTask with external cancellation token support.
+        /// </summary>
+        /// <typeparam name="T">The type of the task result.</typeparam>
+        /// <param name="task">The Task to convert.</param>
+        /// <param name="token">The cancellation token to attach.</param>
+        /// <returns>A UniTask that can be canceled via the provided token.</returns>
+        [UsedImplicitly]
+        public static UniTask<T> AsUniTask<T>(this Task<T> task, CancellationToken token) =>
+            task.AsUniTask().AttachExternalCancellation(token);
+
+        /// <summary>
+        /// Converts a Task to UniTask with external cancellation token support.
+        /// </summary>
+        /// <param name="task">The Task to convert.</param>
+        /// <param name="token">The cancellation token to attach.</param>
+        /// <returns>A UniTask that can be canceled via the provided token.</returns>
+        [UsedImplicitly]
+        public static UniTask AsUniTask(this Task task, CancellationToken token) =>
+            task.AsUniTask().AttachExternalCancellation(token);
+    }
+}
