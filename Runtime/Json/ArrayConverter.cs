b85a10c5a83746c1559f19e02dad2a08c13df72e
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-28 18:47:51 +0300

    [feat] ArrayConverter

---
+++ Runtime/Json/ArrayConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace CustomUtils.Runtime.Json
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// JSON converter for array wrapper types that implement
+    /// <see cref="T:CustomUtils.Runtime.Json.IWrapper`1">IWrapper{T}</see>.
+    /// </summary>
+    /// <typeparam name="TWrapper">The wrapper type that implements
+    /// <see cref="T:CustomUtils.Runtime.Json.IWrapper`1">IWrapper{TData}</see>.</typeparam>
+    /// <typeparam name="TData">The type of data stored in the array.</typeparam>
+    [UsedImplicitly]
+    public sealed class ArrayConverter<TWrapper, TData> : JsonConverter<TWrapper>
+        where TWrapper : IWrapper<TData>, new()
+    {
+        public override void WriteJson(JsonWriter writer, TWrapper value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value.Items);
+        }
+
+        public override TWrapper ReadJson(
+            JsonReader reader,
+            Type objectType,
+            TWrapper existingValue,
+            bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            var array = serializer.Deserialize<TData[]>(reader);
+            var wrapper = new TWrapper();
+            wrapper.SetItems(array);
+            return wrapper;
+        }
+    }
+}
---
+++ Runtime/Json/IWrapper.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+
+namespace CustomUtils.Runtime.Json
+{
+    /// <summary>
+    /// Defines a wrapper for array data that can be serialized and deserialized.
+    /// </summary>
+    /// <typeparam name="T">The type of items in the array.</typeparam>
+    [UsedImplicitly]
+    public interface IWrapper<T>
+    {
+        /// <summary>
+        /// Gets the array of items.
+        /// </summary>
+        [UsedImplicitly]
+        T[] Items { get; }
+
+        /// <summary>
+        /// Sets the array of items.
+        /// </summary>
+        /// <param name="items">The array of items to set.</param>
+        [UsedImplicitly]
+        void SetItems(T[] items);
+    }
+}
