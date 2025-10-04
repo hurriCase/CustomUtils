2140602c2f45b9c5539168a004fd10ba85d82fd0
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-10-04 17:31:31 +0300

    [feat] where observable extension overloads

--- Runtime/Extensions/Observables/ObservableExtensions.Where.cs
+++ Runtime/Extensions/Observables/ObservableExtensions.Where.cs
@@ -28,5 +28,70 @@ namespace CustomUtils.Runtime.Extensions.Observables
         {
             return observable.Where((self, predicate), static (_, tuple) => tuple.predicate(tuple.self));
         }
+
+        /// <summary>
+        /// Filters observable based on predicate using observable value and MonoBehaviour instance.
+        /// </summary>
+        /// <typeparam name="TSelf">MonoBehaviour type.</typeparam>
+        /// <typeparam name="T">Observable value type.</typeparam>
+        /// <param name="observable">Observable to filter.</param>
+        /// <param name="self">MonoBehaviour instance for predicate.</param>
+        /// <param name="predicate">Predicate function that takes observable value and MonoBehaviour instance.</param>
+        /// <returns>Filtered observable.</returns>
+        [UsedImplicitly]
+        public static Observable<T> Where<TSelf, T>(
+            this Observable<T> observable,
+            TSelf self,
+            Func<T, TSelf, bool> predicate)
+            where TSelf : MonoBehaviour
+        {
+            return observable.Where((self, predicate), static (value, tuple) => tuple.predicate(value, tuple.self));
+        }
+
+        /// <summary>
+        /// Filters observable based on predicate using additional data and MonoBehaviour instance.
+        /// </summary>
+        /// <typeparam name="TSelf">MonoBehaviour type.</typeparam>
+        /// <typeparam name="T">Observable value type.</typeparam>
+        /// <typeparam name="TTuple">Additional data type.</typeparam>
+        /// <param name="observable">Observable to filter.</param>
+        /// <param name="self">MonoBehaviour instance for predicate.</param>
+        /// <param name="tuple">Additional data passed to the predicate.</param>
+        /// <param name="predicate">Predicate function that takes additional data and MonoBehaviour instance.</param>
+        /// <returns>Filtered observable.</returns>
+        [UsedImplicitly]
+        public static Observable<T> Where<TSelf, T, TTuple>(
+            this Observable<T> observable,
+            TSelf self,
+            TTuple tuple,
+            Func<TTuple, TSelf, bool> predicate)
+            where TSelf : MonoBehaviour
+        {
+            return observable.Where((self, tuple, predicate),
+                static (_, state) => state.predicate(state.tuple, state.self));
+        }
+
+        /// <summary>
+        /// Filters observable based on predicate using observable value, MonoBehaviour instance, and additional data.
+        /// </summary>
+        /// <typeparam name="TSelf">MonoBehaviour type.</typeparam>
+        /// <typeparam name="T">Observable value type.</typeparam>
+        /// <typeparam name="TTuple">Additional data type.</typeparam>
+        /// <param name="observable">Observable to filter.</param>
+        /// <param name="self">MonoBehaviour instance for predicate.</param>
+        /// <param name="tuple">Additional data passed to the predicate.</param>
+        /// <param name="predicate">Predicate function that takes observable value, MonoBehaviour instance, and additional data.</param>
+        /// <returns>Filtered observable.</returns>
+        [UsedImplicitly]
+        public static Observable<T> Where<TSelf, T, TTuple>(
+            this Observable<T> observable,
+            TSelf self,
+            TTuple tuple,
+            Func<T, TSelf, TTuple, bool> predicate)
+            where TSelf : MonoBehaviour
+        {
+            return observable.Where((self, tuple, predicate),
+                static (value, state) => state.predicate(value, state.self, state.tuple));
+        }
     }
 }
