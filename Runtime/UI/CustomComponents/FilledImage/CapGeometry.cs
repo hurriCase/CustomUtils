587f92e8b19a249b76c9183be028b29dc71deea4
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-10-16 11:40:36 +0300

    [refactor] cap geometry calculation simplification

--- Runtime/UI/CustomComponents/FilledImage/CapGeometry.cs
+++ Runtime/UI/CustomComponents/FilledImage/CapGeometry.cs
@@ -8,36 +8,34 @@ namespace CustomUtils.Runtime.UI.CustomComponents.FilledImage
         internal Vector2[] CapPoints { get; }
         internal bool HasCap => CapPoints?.Length > 0;
 
-        private const float HalfCircleRadians = Mathf.PI * 0.5f;
-
         private CapGeometry(Vector2[] points)
         {
             CapPoints = points;
         }
 
-        internal static CapGeometry CreateStartCap(bool isEnabled, CapParameters parameters, float radians) =>
+        internal static CapGeometry CreateStartCap(bool isEnabled, CapParameters parameters, float startRadians) =>
             isEnabled
-                ? CreateCap(parameters, radians, -HalfCircleRadians)
+                ? CreateCap(parameters, startRadians, -Mathf.PI)
                 : default;
 
-        internal static CapGeometry CreateEndCap(bool isEnabled, CapParameters parameters, float radians) =>
+        internal static CapGeometry CreateEndCap(bool isEnabled, CapParameters parameters, float endRadians) =>
             isEnabled
-                ? CreateCap(parameters, radians, HalfCircleRadians)
+                ? CreateCap(parameters, endRadians, Mathf.PI)
                 : default;
 
         private static CapGeometry CreateCap(CapParameters parameters, float radians, float initialOffset)
         {
             var points = new Vector2[parameters.Resolution + 1];
-            var middleRadius = parameters.InnerRadius + (parameters.OuterRadius - parameters.InnerRadius) * 0.5f;
+            var capRadius = (parameters.OuterRadius - parameters.InnerRadius) * 0.5f;
+            var middleRadius = parameters.InnerRadius + capRadius;
             var direction = radians.GetDirectionFromAngle();
             var centerPoint = parameters.Center + direction * middleRadius;
-            var startAngle = radians + initialOffset;
-            var capRadius = (parameters.OuterRadius - parameters.InnerRadius) * 0.5f;
+            var capAngleStart = radians + initialOffset;
 
             for (var i = 0; i <= parameters.Resolution; i++)
             {
                 var t = (float)i / parameters.Resolution;
-                var angle = startAngle + Mathf.Lerp(-HalfCircleRadians, HalfCircleRadians, t);
+                var angle = Mathf.Lerp(capAngleStart, radians, t);
                 points[i] = centerPoint + angle.GetDirectionFromAngle() * capRadius;
             }
 
