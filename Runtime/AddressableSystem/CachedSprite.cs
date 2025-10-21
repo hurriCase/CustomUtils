f03f4966647336a9059c1b7ae51b35101cfba2f4
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-10-21 18:27:23 +0300

    [feat] FromPath in CachedSprite

--- Runtime/AddressableSystem/CachedSprite.cs
+++ Runtime/AddressableSystem/CachedSprite.cs
@@ -2,6 +2,10 @@
 using MemoryPack;
 using UnityEngine.AddressableAssets;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace CustomUtils.Runtime.AddressableSystem
 {
     /// <summary>
@@ -30,6 +34,21 @@ namespace CustomUtils.Runtime.AddressableSystem
         /// <param name="assetReference">The asset reference to cache.</param>
         [UsedImplicitly]
         public CachedSprite(AssetReference assetReference) => AssetGUID = assetReference.AssetGUID;
+
+        /// <summary>
+        /// Creates a CachedSprite from an asset path by resolving its GUID.
+        /// </summary>
+        /// <param name="assetPath">The asset path (e.g., "Assets/Sprites/icon.png").</param>
+        [UsedImplicitly]
+        public static CachedSprite FromPath(string assetPath)
+        {
+#if UNITY_EDITOR
+            var guid = AssetDatabase.AssetPathToGUID(assetPath);
+            return new CachedSprite(guid);
+#else
+            return default;
+#endif
+        }
 
         /// <summary>
         /// Gets whether the cached sprite has a valid GUID.
