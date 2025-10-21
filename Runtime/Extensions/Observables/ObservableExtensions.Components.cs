6acc31bf2e1cd4b222bbacd4b730893cbed60fad
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-10-21 17:57:57 +0300

    [feat] ObservableExtensions

---
+++ Runtime/Extensions/Observables/ObservableExtensions.Components.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using R3;
+using TMPro;
+
+namespace CustomUtils.Runtime.Extensions.Observables
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="Observable{T}"/> to work with UI components.
+    /// </summary>
+    [UsedImplicitly]
+    public partial class ObservableExtensions
+    {
+        /// <summary>
+        /// Converts the value change events of a <see cref="TMP_InputField"/> into an observable sequence.
+        /// Emits the current value of the input field and subsequent value changes as they occur.
+        /// </summary>
+        /// <param name="inputField">
+        /// The <see cref="TMP_InputField"/> whose value change events are monitored and transformed into an observable sequence.
+        /// </param>
+        /// <returns>
+        /// An observable sequence that emits the current and updated string values of the input field as they change.
+        /// </returns>
+        [UsedImplicitly]
+        public static Observable<string> OnValueChangedAsObservable(this TMP_InputField inputField)
+        {
+            return Observable.Create<string, TMP_InputField>(inputField, static (observer, inputField) =>
+            {
+                observer.OnNext(inputField.text);
+                return inputField.onValueChanged.AsObservable(inputField.destroyCancellationToken)
+                    .Subscribe(observer);
+            });
+        }
+    }
+}
