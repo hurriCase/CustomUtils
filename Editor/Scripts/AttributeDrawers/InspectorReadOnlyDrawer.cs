2fdb62a2ffcd110fe347de83e4d6b800008bbcf5
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-10-24 07:45:10 +0300

    [fix] InspectorReadOnlyDrawer.cs property height

--- Editor/Scripts/AttributeDrawers/InspectorReadOnlyDrawer.cs
+++ Editor/Scripts/AttributeDrawers/InspectorReadOnlyDrawer.cs
@@ -7,6 +7,9 @@ namespace CustomUtils.Editor.Scripts.AttributeDrawers
     [CustomPropertyDrawer(typeof(InspectorReadOnlyAttribute))]
     internal class InspectorReadOnlyDrawer : PropertyDrawer
     {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+            => EditorGUI.GetPropertyHeight(property, label, true);
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             GUI.enabled = false;
