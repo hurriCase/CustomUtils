4d953532e3ace19689247cc06c43571b01f61315
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-11-06 15:14:49 +0300

    [feat] AddUnityFileStyles extension method

--- Runtime/Extensions/BasePopupFieldExtensions.cs
+++ Runtime/Extensions/BasePopupFieldExtensions.cs
@@ -5,25 +5,24 @@ using UnityEngine.UIElements;
 namespace CustomUtils.Runtime.Extensions
 {
     /// <summary>
-    /// Provides extension methods for <see cref="BasePopupField{TValueType, TValueChoice}"/>.
+    /// Provides extension methods for <see cref="VisualElement"/> and related types.
     /// </summary>
     [UsedImplicitly]
     public static class BasePopupFieldExtensions
     {
-        private const string UnityInputFieldUSSClass = "unity-popup-field__input";
+        private const string UnityInspectorFieldUssClassName = "unity-inspector-field";
 
         /// <summary>
         /// Registers a click action on the popup field input element.
         /// </summary>
         /// <typeparam name="TValueType">The type of the value of the popup field.</typeparam>
-        /// <typeparam name="TValueChoice">The type of the choices in the popup field.</typeparam>
         /// <typeparam name="TSource">The type of the source object.</typeparam>
         /// <param name="field">The popup field to register the click action on.</param>
         /// <param name="source">The source object to pass to the click action.</param>
         /// <param name="clickAction">The action to invoke when the input is clicked.</param>
         [UsedImplicitly]
-        public static void RegisterInputClick<TValueType, TValueChoice, TSource>(
-            this BasePopupField<TValueType, TValueChoice> field,
+        public static void RegisterInputClick<TValueType, TSource>(
+            this PopupField<TValueType> field,
             TSource source,
             Action<TSource> clickAction)
         {
@@ -35,8 +34,20 @@ namespace CustomUtils.Runtime.Extensions
             var label = field.Q<Label>();
             label.pickingMode = PickingMode.Ignore;
 
-            var dropdownInput = field.Q(className: UnityInputFieldUSSClass);
+            var dropdownInput = field.Q(className: PopupField<string>.inputUssClassName);
             dropdownInput.pickingMode = PickingMode.Position;
+        }
+
+        /// <summary>
+        /// Adds Unity inspector field styling classes to the field.
+        /// </summary>
+        /// <typeparam name="TValueType">The type of the value of the field.</typeparam>
+        /// <param name="field">The field to add styling classes to.</param>
+        [UsedImplicitly]
+        public static void AddUnityFileStyles<TValueType>(this BaseField<TValueType> field)
+        {
+            field.AddToClassList(BaseField<TValueType>.alignedFieldUssClassName);
+            field.AddToClassList(UnityInspectorFieldUssClassName);
         }
     }
 }
