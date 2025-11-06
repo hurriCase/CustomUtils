231278196c2591e33320b35e98449b8ac389d14a
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-11-06 20:40:44 +0300

    [fix] LocalizationRegistry.cs search/hashes handling

--- Runtime/Localization/LocalizationRegistry.cs
+++ Runtime/Localization/LocalizationRegistry.cs
@@ -19,32 +19,32 @@ namespace CustomUtils.Runtime.Localization
     {
         [field: SerializeField]
         internal SerializedDictionary<string, LocalizationEntry> Entries { get; private set; } = new();
-
-        [field: SerializeField]
-        internal SerializedDictionary<string, HashSet<string>> TableToGuids { get; private set; } = new();
-
         [field: SerializeField] public List<SystemLanguage> SupportedLanguages { get; set; } = new();
+
+        internal IReadOnlyDictionary<string, HashSet<string>> TableToGuids => _tableToGuids;
+
+        private readonly Dictionary<string, HashSet<string>> _tableToGuids = new();
 
         internal void AddOrUpdateEntry(LocalizationEntry entry)
         {
             Entries[entry.GUID] = entry;
 
-            if (TableToGuids.ContainsKey(entry.TableName) is false)
-                TableToGuids[entry.TableName] = new HashSet<string>();
+            if (_tableToGuids.ContainsKey(entry.TableName) is false)
+                _tableToGuids[entry.TableName] = new HashSet<string>();
 
-            TableToGuids[entry.TableName].Add(entry.GUID);
+            _tableToGuids[entry.TableName].Add(entry.GUID);
 
             this.MarkAsDirty();
         }
 
         internal IList SearchEntries(string searchText, string tableName = null)
         {
-            if (string.IsNullOrEmpty(searchText))
-                return Entries.Values.ToArray();
-
             var entriesToSearch = string.IsNullOrEmpty(tableName)
                 ? Entries.Values
                 : GetEntriesForTable(tableName);
+
+            if (string.IsNullOrEmpty(searchText))
+                return entriesToSearch.ToArray();
 
             return entriesToSearch
                 .Where(entry => entry.Key.Contains(searchText, StringComparison.OrdinalIgnoreCase))
@@ -54,12 +54,12 @@ namespace CustomUtils.Runtime.Localization
         internal void Clear()
         {
             Entries.Clear();
-            TableToGuids.Clear();
+            _tableToGuids.Clear();
             this.MarkAsDirty();
         }
 
         private IReadOnlyCollection<LocalizationEntry> GetEntriesForTable(string tableName) =>
-            TableToGuids.TryGetValue(tableName, out var guids)
+            _tableToGuids.TryGetValue(tableName, out var guids)
                 ? guids.Select(guid => Entries[guid]).ToArray()
                 : Array.Empty<LocalizationEntry>();
     }
