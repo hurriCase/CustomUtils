85b9bdc121e7b4db5168cfb6b9d2c00e510ed8e4
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-11-07 18:48:27 +0300

    [feat] LocalizationKeyExtensions

---
+++ Runtime/Extensions/LocalizatioKeyExtensions.cs
@@ -0,0 +1,20 @@
+﻿using CustomUtils.Runtime.Localization;
+using JetBrains.Annotations;
+
+namespace CustomUtils.Runtime.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="LocalizationKey"/>.
+    /// </summary>
+    [UsedImplicitly]
+    public static class LocalizationKeyExtensions
+    {
+        /// <summary>
+        /// Gets the localized string for the specified localization key.
+        /// </summary>
+        /// <param name="key">The localization key to get the localized string for.</param>
+        /// <returns>The localized string corresponding to the key.</returns>
+        [UsedImplicitly]
+        public static string GetLocalization(this LocalizationKey key) => LocalizationController.Localize(key);
+    }
+}
