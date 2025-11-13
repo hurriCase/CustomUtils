ebe75e51ee9520a12828f162e55f5be14d7f4041
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-11-13 09:06:03 +0300

    [feat] DayInMonths in Date.cs

--- Runtime/Constants/Date.cs
+++ Runtime/Constants/Date.cs
@@ -10,6 +10,7 @@ namespace CustomUtils.Runtime.Constants
     public static class Date
     {
         public const int DaysInCalendar = 42;
+        public const int DayInMonths = 30;
         public const int DaysPerWeek = 7;
 
         public static DateOnly Today => DateOnly.FromDateTime(DateTime.Now);
