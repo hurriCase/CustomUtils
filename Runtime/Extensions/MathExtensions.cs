74604a2ba6ecf41b39256fb6eb46583993d6b618
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-11-27 19:00:58 +0300

    [feat] GetInitialVelocity extension method

--- Runtime/Extensions/MathExtensions.cs
+++ Runtime/Extensions/MathExtensions.cs
@@ -6,7 +6,7 @@ namespace CustomUtils.Runtime.Extensions
     /// <summary>
     /// Provides extension methods for mathematical operations.
     /// </summary>
-    [UsedImplicitly]
+    [PublicAPI]
     public static class MathExtensions
     {
         private const float LowerThreshold = 0.1f;
@@ -22,7 +22,6 @@ namespace CustomUtils.Runtime.Extensions
         /// <param name="value">The float value to evaluate.</param>
         /// <returns>True if the value is greater than the lower threshold and less than the upper threshold;
         /// otherwise, false.</returns>
-        [UsedImplicitly]
         public static bool IsReasonable(this float value) => value is > LowerThreshold and < UpperThreshold;
 
         /// <summary>
@@ -30,7 +29,6 @@ namespace CustomUtils.Runtime.Extensions
         /// </summary>
         /// <param name="vector4">The Vector4 instance to be modified.</param>
         /// <returns>A new Vector4 instance with all components clamped to zero or higher.</returns>
-        [UsedImplicitly]
         public static Vector4 ClampToPositive(this Vector4 vector4)
         {
             vector4.x = Mathf.Max(vector4.x, 0);
@@ -43,7 +41,6 @@ namespace CustomUtils.Runtime.Extensions
         /// <summary>
         /// Finds the minimum value from multiple float values without allocating memory.
         /// </summary>
-        [UsedImplicitly]
         public static float MinOf(float value1, float value2, float value3, float value4, float value5)
             => Mathf.Min(Mathf.Min(Mathf.Min(Mathf.Min(value1, value2), value3), value4), value5);
 
@@ -55,7 +52,6 @@ namespace CustomUtils.Runtime.Extensions
         /// <c>true</c> if the specified integer is a power of two (2, 4, 8, 16, etc.);
         /// <c>false</c> if the integer is zero or not a power of two.
         /// </returns>
-        [UsedImplicitly]
         public static bool IsPowerOfTwo(this int x) => x != 0 && (x & (x - 1)) == 0;
 
         /// <summary>
@@ -65,7 +61,6 @@ namespace CustomUtils.Runtime.Extensions
         /// <param name="rect">The rectangle bounds</param>
         /// <param name="values">Vector4 values to scale (x,y pair and z,w pair for width; x,w pair and z,y pair for height)</param>
         /// <returns>Scale factor to apply (clamped to maximum of 1.0)</returns>
-        [UsedImplicitly]
         public static float CalculateScaleFactorForBounds(this Rect rect, Vector4 values) =>
             MinOf(
                 rect.width / (values.x + values.y),  // First width constraint
@@ -81,7 +76,6 @@ namespace CustomUtils.Runtime.Extensions
         /// <param name="a">The first floating point number to encode.</param>
         /// <param name="b">The second floating point number to encode.</param>
         /// <returns>A float value representing the encoded values of <paramref name="a"/> and <paramref name="b"/>.</returns>
-        [UsedImplicitly]
         public static float PackAs16BitWith(this float a, float b)
         {
             var encodedValues = new Vector2(
@@ -97,7 +91,6 @@ namespace CustomUtils.Runtime.Extensions
         /// </summary>
         /// <param name="degrees">The angle in degrees to convert.</param>
         /// <returns>The equivalent angle in radians.</returns>
-        [UsedImplicitly]
         public static float ToRadians(this float degrees) => degrees * Mathf.Deg2Rad;
 
         /// <summary>
@@ -105,8 +98,15 @@ namespace CustomUtils.Runtime.Extensions
         /// </summary>
         /// <param name="angleInRadians">The angle in radians for which the direction vector is calculated.</param>
         /// <returns>A 2D vector representing the direction corresponding to the given angle.</returns>
-        [UsedImplicitly]
         public static Vector2 GetDirectionFromAngle(this float angleInRadians)
             => new(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians));
+
+        /// <summary>
+        /// Calculates the initial upward velocity required to reach a specified vertical displacement.
+        /// </summary>
+        /// <param name="displacement">The desired vertical displacement in meters.</param>
+        /// <returns>The initial velocity needed to achieve the displacement.</returns>
+        public static float GetInitialVelocity(this float displacement)
+            => Mathf.Sqrt(displacement * -2f * Physics.gravity.y);
     }
 }
