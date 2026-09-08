7fb77199d477315b763ce41ebb6a80e3fe09b1a8
hurriCase <dmitriy.pastanovskiy@gmail.com>
2026-09-08 12:21:44 +0300

    refactor: keep only plain Dictionary version in GradientConfig

--- Runtime/UI/Theme/ColorModifiers/GradientModifier/ShaderGradient/GradientConfig.cs
+++ Runtime/UI/Theme/ColorModifiers/GradientModifier/ShaderGradient/GradientConfig.cs
@@ -2,22 +2,13 @@
 using CustomUtils.Runtime.CustomTypes.Singletons;
 using UnityEngine;
 
-#if UNITY_6000_7_OR_NEWER
 using System.Collections.Generic;
-
-#else
-using AYellowpaper.SerializedCollections;
-#endif
 
 namespace CustomUtils.Runtime.UI.Theme.ColorModifiers.GradientModifier.ShaderGradient
 {
     [Resource(name: nameof(GradientConfig))]
     internal sealed class GradientConfig : SingletonScriptableObject<GradientConfig>
     {
-#if UNITY_6000_7_OR_NEWER
         [field: SerializeField] internal Dictionary<GradientType, string> GradientKeywords { get; set; }
-#else
-        [field: SerializeField] internal SerializedDictionary<GradientType, string> GradientKeywords { get; set; }
-#endif
     }
 }
